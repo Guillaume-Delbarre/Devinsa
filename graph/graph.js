36b4b1d38be62851da9f2b2fcc3dc60eb43099d3
Guillaume <guillaume.voolkan@mgmail.com>
2019-12-23 13:34:06 +0100

    Amélioration du graph

---
+++ graph/graph.js
@@ -0,0 +1,133 @@
+(function (d3) {
+  'use strict';
+  
+  const svg = d3.select('svg');
+
+  const width = +svg.attr('width');
+  const height = +svg.attr('height');
+  
+  const render = data => {
+    
+    const title = 'Graph PCA';
+    
+    const xValue = d => d.Axe_X;
+    const xAxisLabel = 'Axe 1';
+    
+    const yValue = d => d.Axe_Y;
+    const yAxisLabel = 'Axe 2';
+    
+    const circleRadius = 8;
+    
+    const margin = { top: 60, right: 40, bottom: 88, left: 150 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    
+    const xScale = d3.scaleLinear()
+      .domain(d3.extent(data, xValue))
+      .range([0, innerWidth])
+      .nice();
+    
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(data, yValue))
+      .range([innerHeight, 0])
+      .nice();
+    
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+    
+    const xAxis = d3.axisBottom(xScale)
+      .tickSize(-innerHeight)
+      .tickPadding(15);
+    
+    const yAxis = d3.axisLeft(yScale)
+      .tickSize(-innerWidth)
+      .tickPadding(10);
+    
+    const yAxisG = g.append('g').call(yAxis);
+    
+    yAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', -65)
+        .attr('x', -innerHeight / 2)
+        .attr('fill', 'black')
+        .attr('transform', `rotate(-90)`)
+        .attr('text-anchor', 'middle')
+        .text(yAxisLabel);
+    
+    const xAxisG = g.append('g').call(xAxis)
+      .attr('transform', `translate(0,${innerHeight})`);
+    
+    xAxisG.select('.domain').remove();
+    
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 85)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'black')
+        .text(xAxisLabel);
+    
+    var zoom = d3.zoom()
+    	.scaleExtent([.5,20])
+    	.extent([[0,0], [innerWidth,innerHeight]])
+    	.on('zoom', updateZoom);
+    
+    g.append('rect')
+    	.attr('class', 'zoomRect')
+    	.attr('width', innerWidth)
+    	.attr('height', innerHeight)
+    	.call(zoom);
+    
+    g.append('clipPath')
+    	.attr('id', 'rect-clip')
+    .append('rect')
+    	.attr('width', innerWidth)
+    	.attr('height', innerHeight);
+    
+    g.selectAll('circle').data(data)
+      .enter().append('circle')
+    		.attr('clip-path', 'url(#rect-clip)')
+    		.attr('class', 'myCircle')
+        .attr('cy', d => yScale(yValue(d)))
+        .attr('cx', d => xScale(xValue(d)))
+        .attr('r', circleRadius)
+    	.append('title')
+    		.text(d => d.Name);
+    
+    g.append('text')
+        .attr('class', 'title')
+    		.attr('x', 100)
+        .attr('y', -10)
+        .text(title);
+    
+    function updateZoom() {
+
+      var newX = d3.event.transform.rescaleX(xScale);
+      var newY = d3.event.transform.rescaleY(yScale);
+      
+      var newXAxis = d3.axisBottom(newX)
+      	.tickSize(-innerHeight)
+      	.tickPadding(15);
+      
+      const newYAxis = d3.axisLeft(newY)
+      	.tickSize(-innerWidth)
+      	.tickPadding(10);
+			
+      xAxisG.call(newXAxis);
+      yAxisG.call(newYAxis);
+
+      g.selectAll('circle')
+        .attr("cx", function(d) {return newX(d.Axe_X)})
+        .attr("cy", function(d) {return newY(d.Axe_Y)});
+    }
+  
+  };
+  
+  d3.csv('https://raw.githubusercontent.com/Guillaume-Delbarre/Devinsa/master/resPCA.csv')
+  	.then(data => {
+    	data.forEach(d => {
+        d.Axe_X = +d.Axe_X;
+        d.Axe_Y = +d.Axe_Y;
+      });
+    	render(data);
+  });
+}(d3));
