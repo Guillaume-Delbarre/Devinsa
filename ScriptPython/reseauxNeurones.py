51ca9d40fa6c59c448d082ba53c5182ba8c86c65
zoepj <zoe.pesleux@insa-rennes.fr>
2020-02-03 20:04:07 +0100

    reseauxNeurones.py -> fct autoencoder2D

--- ScriptPython/reseauxNeurones.py
+++ ScriptPython/reseauxNeurones.py
@@ -5,26 +5,11 @@ Created on Mon Jan 27 12:17:33 2020
 @author: pjzoe
 """
 import pandas as pd
-from keras.layers import Dense, Activation, Input
+from keras.layers import Dense, Input
 from keras.models import Model
-from keras.optimizers import SGD
-import numpy as np
-import tensorflow as tf
 
 
-
-#def modelePourVisualisation():
- #   entree = Input(shape=(902,))
-  #  inter = Dense(units=100, activation='relu')(entree)
-   # enc = Dense(units=2, activation ='linear')(inter)
-    #inter = Dense(units=100, activation='relu')(enc)
-    #sortie = Dense(units=902, activation ='linear')(inter)
-    #model = Model(inputs=entree,outputs=sortie)
-    #model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
-    #return model
-
-
-def main():
+def autoencoder2D():
     df = pd.read_csv("C:/Users/pjzoe/Documents/3INFO/Etudes pratiques/GitHub/Données/Personnages.csv", sep = ";", header=0, index_col=0, encoding = 'latin1')
     df = df.to_numpy()
     print(df.shape)
@@ -43,10 +28,7 @@ def main():
     enc = Model(inputs=entree,outputs=enc)
     enc.compile(loss='mse',optimizer='adam',metrics=['mae'])
     autoEnc.summary()
-    print('df.shape=',df.shape)
-    #x_train=x_train.reshape(1430,902)
     autoEnc.fit(df,df,epochs=20,batch_size=4)
-    res = enc.predict(df)
+    return enc.predict(df)
     
-if __name__ == "__main__":
-	main()
+
