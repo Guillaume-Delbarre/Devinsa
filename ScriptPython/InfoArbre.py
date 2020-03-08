2578705f09e1d9db71fd2e4a3b0f7132e41de659
hugo.martineau <hugo.martineau3@gmail.com>
2020-03-08 14:32:23 +0100

    Arbre fini 1.0

--- ScriptPython/InfoArbre.py
+++ ScriptPython/InfoArbre.py
@@ -9,7 +9,7 @@ Created on Wed Jan  04 14:32:09 2020
 import csv
 
 global TAUX
-TAUX = 0.75
+TAUX = 0.25
 
 def extraitMatricePersonnage(file):
     results = []
@@ -52,7 +52,6 @@ def avoirRangQuestion(question,matricePersoQuestion):
             return i
     return -1
 
-
 def obtenirRangQuestionsFilles(parentid,matriceQuestion):
     verif = 0
     res = []
@@ -74,12 +73,56 @@ def compterPerso(rangQuestion,matricePerso):
     res = recopieMatrice(matricePerso)
     for i in range(1,len(matricePerso)):
         idf = matricePerso[i][rangQuestion]
-        if(float(idf)>TAUX):
+        if(float(idf)<TAUX):
             res.remove(matricePerso[i])
     return res
 
+def median(matrice):
+    med = [0]
+    summ = 0
+    for j in range(1,len(matrice[0])):
+        for i in range(1,len(matrice)):
+           summ += float(matrice[i][j])
+        summ = summ/(len(matrice)-1)
+        med.append(summ)
+        summ = 0
+    return med
+
+def proxi(med,matrice):
+    dist_aux = 0
+    for j in range(1,len(matrice[0])):
+        dist_aux += carre(med[j]-float(matrice[1][j]))
+    dist = dist_aux
+    dist_aux = 0
+    rang = 1
+    for i in range(2,len(matrice)):
+        for j in range(2,len(matrice[0])):
+            dist_aux += carre(med[j]-float(matrice[i][j]))
+        if(dist_aux<dist):
+            dist = dist_aux
+            rang = i
+        dist_aux = 0
+    return rang
+
+def carre(x):
+    return (x)*(x)
+
+def guillemet(s):
+    i = 0
+    taille = len(s)
+    while(i<taille):
+        if(s[i]=="'"):
+            s = s[:i]+"'+"+s[i:]
+            i += 2
+        i+=1
+    return s
+                    
 def elagagePerso(question,matriceArbre,matricePerso,res):
-    res.append([question[2],len(matricePerso)])
+    if(len(matricePerso)==1):
+        res.append([question[2],0,'Aucun'])
+    else:
+        rangMedian = proxi(median(matricePerso),matricePerso)
+        res.append([question[2],len(matricePerso)-1,matricePerso[rangMedian][0]])
     rangQuestionsFilles = obtenirRangQuestionsFilles(question[2],matriceArbre)
     if(rangQuestionsFilles==-1):
         return
@@ -91,8 +134,8 @@ def elagagePerso(question,matriceArbre,matricePerso,res):
     choixNon = rangQuestionsFilles[1]
     rangQuestionOui = avoirRangQuestion(matriceArbre[choixOui][0],matricePerso)
     rangQuestionNon = avoirRangQuestion(matriceArbre[choixNon][0],matricePerso)
-    matricePersoNon = compterPerso(rangQuestion, matricePerso)
-    matricePersoOui = compterPerso(rangQuestion+1, matricePerso)
+    matricePersoOui = compterPerso(rangQuestion, matricePerso)
+    matricePersoNon = compterPerso(rangQuestion+1, matricePerso)
     elagagePerso(matriceArbre[choixNon],matriceArbre,matricePersoNon,res)
     elagagePerso(matriceArbre[choixOui],matriceArbre,matricePersoOui,res)
     return res
--- ScriptPython/apptree.py
+++ ScriptPython/apptree.py
@@ -32,13 +32,13 @@ def ecrirejstree(resultat, filesortie):
 def persoRestant(idquestion,elagage):
     for i in range(len(elagage)):
         if(idquestion==elagage[i][0]):
-            return elagage[i][1]
+            return elagage[i]
     return -1
 
 def ecrirejstreeV2(resultat,perso,filesortie):
     ecriture = open(filesortie,"w",encoding="utf-8")
     elagage = elagagePerso(resultat[0],resultat,perso,[])
-    ecriture.write("questionid_1 = {text: { name: '"+resultat[0][0]+"', desc : 'Personnages restants : "+str(elagage[0][1])+"' }, collapsed : true};\n")
+    ecriture.write("questionid_1 = {text: { name: '"+resultat[0][0]+"', desc : 'Personnages restants : "+str(elagage[0][1])+" Personnage médian :"+str(elagage[0][2])+"' }, collapsed : true};\n")
     chart_config = "chart_config = [\n{container: '#basic-example',\nconnectors: { type: 'step' },\n node: { HTMLclass: 'nodeExample1' },\n animation: { nodeAnimation: "+'"'+"easeOutBounce"+'"'+", nodeSpeed: 700,connectorsAnimation: "+'"'+"bounce"+'"'+", connectorsSpeed: 700 }},\n questionid_1,"
     for questions in resultat:   
         parentid = questions[2]
@@ -51,8 +51,10 @@ def ecrirejstreeV2(resultat,perso,filesortie):
                 choix = "Non"
             titre = fils[1]
             chart_config += "questionid_"+fils[2]+",\n"
-            rest = persoRestant(fils[2],elagage)
-            ecriture.write("questionid_"+fils[2]+" = {parent: questionid_"+parentid+",text: { name: 'Choix : "+choix+"', desc : 'Titre : "+titre+" Personnages restants : "+str(rest)+"' }, collapsed : true};\n")
+            info = persoRestant(fils[2],elagage)
+            rest = info[1]
+            median = guillemet(info[2])
+            ecriture.write("questionid_"+fils[2]+" = {parent: questionid_"+parentid+",text: { name: 'Choix : "+choix+"', desc : 'Titre : "+titre+" Personnages restants : "+str(rest)+" Personnage médian :"+str(median)+"'}, collapsed : true};\n")
     chart_config = chart_config[0:len(chart_config)-2]
     chart_config += "];"
     ecriture.write(chart_config)
