1a0bcbcd27bcba05b173c9829f321a9a25bbe4a8
zoepj <zoe.pesleux@insa-rennes.fr>
2020-03-10 13:37:37 +0100

    Correctif ordre cluster

--- ScriptPython/questionsCaracteristiques.py
+++ ScriptPython/questionsCaracteristiques.py
@@ -2,18 +2,21 @@
 
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import pdist, cdist
 
 medoids = None
 
 
 def ecritQuestionCarac(df,agg,nbCluster,nbQuestion):
     file = open("../Donnees/infoClusters.csv","w",encoding='utf-8')
+    #ecriture de l'entête
     file.write("Cluster,Medoid,")
     for i in range(nbQuestion-1):
         file.write("Q"+str(i)+",")
     file.write("Q"+str(nbQuestion-1)+"\n")
 
     for i in range(1,nbCluster+1):
+        #Pour un cluster donné, on ordonne inversement les question en fonction du score TF-IDF
         agg_sorted = agg.sort_values(by=i, axis=1, ascending=False)
         file.write(str(i)+","+medoids[i-1]+",")
         j=0
@@ -29,12 +32,19 @@ def getQCarac(nbCluster=6, nbQuestion=5):
     global medoids
     df = pd.read_csv("../Donnees/kmeans.csv", sep = ";", header=0, index_col=0, encoding = 'utf-8')
     df.sort_values(by='Clusters', inplace=True)
+    #On récupère les médoides dans un tableau
     medoids = df.loc[df['Medoid']==1].index.values
     
     del df['Medoid']
-    df['Clusters'].replace(0,nbCluster,inplace=True)
+    #Au lieu d'aller de 0 à 5, les clusters iront de 1 à 6
+    for k in range(nbCluster,0,-1):
+        df['Clusters'].replace(k-1,k, inplace=True)
+    #On remplace les 0 (absence de donnees) par NaN
     df = df.replace(0,np.nan)
+    print(df)
+    #On fait la moyenne des TF-IDF pour chaque question par cluster (en ignorant les NaN)
     agg = df.groupby(['Clusters']).mean()
+    #print(cdist(agg[agg.index==1],df[df['Clusters']==1].iloc[0:82,0:902])) Probleme avec les NaN
     ecritQuestionCarac(df,agg,nbCluster,nbQuestion)
     
     
