0fd612a04763dc10f75f443c010e4959ca722f0c
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2020-03-25 19:05:20 +0100

    test pour faire le tableau

--- Web/graph/table.js
+++ Web/graph/table.js
@@ -6,16 +6,22 @@
         var couleur = d3.scaleOrdinal(d3.schemeCategory10)
         .domain(["Groupe 0","Groupe 1","Groupe 2","Groupe 3","Groupe 4","Groupe 5","Groupe 6","Groupe 7","Groupe 8","Groupe 9"])
 
-        var tr = d3.select(".objecttable tbody")
-            .selectAll("tr")
-            .data(data)
-            .enter().append("tr")
+        var table = d3.select(".objecttable tbody")
 
-        var td = tr.selectAll("td")
-            .data(function(d, i) { return Object.values(d); })
-            .enter().append("td")
-                .text(function(d) { return d; })
-                    .attr('fontcolor', d => couleur("Groupe " + d.Cluster));
+        table.append("thead").append("tr")
+            .selectAll('th')
+            .data(function(d,i) {return Object.keys(d);})
+            .enter().append('th')
+                .text(function(d) {return d;})
+
+        table.append('tbody')
+            .selectAll('tr')
+            .data(function(d, i) {return Object.values(d);})
+            .enter().append('tr')
+                .text(function(d) {return d;})
+
+
+        
 
     };
 
