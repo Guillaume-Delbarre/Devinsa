0e5ed2d3ca05e00c47c55a469549ec89eafded18
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-28 20:37:23 +0100

    corr

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -4,16 +4,10 @@ base = mysql.connector.connect(host='localhost',database='devinsa',user='root',p
 
 curseur = base.cursor()
 
-curseur.execute("SELECT yes_count FROM app_answer WHERE item_id = 7336")
-
-for x in curseur:
-    print x
-
-print "\n."
-
-curseur.execute("SELECT id FROM app_question WHERE title = A  t  il  un  rapport  avec  l''espace  ?")
+curseur.execute("SELECT yes_count FROM app_answer WHERE item_id = 7336 and question_id = 1")
 
 for x in curseur:
     print x
 
 print "\nend"
+
