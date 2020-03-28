3f070353386f82acd3d77c36b58804b9aecf7cca
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-28 20:30:34 +0100

    corr

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -11,7 +11,7 @@ for x in curseur:
 
 print "\n."
 
-curseur.execute("SELECT id FROM app_question WHERE title = A t il un rapport avec l'espace ?")
+curseur.execute("SELECT id FROM app_question WHERE title = A t il un rapport avec l%'espace ?")
 
 for x in curseur:
     print x
