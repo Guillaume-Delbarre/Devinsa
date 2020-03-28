f3f647f196578ac6a6dd37af7596987f1910bdcc
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-28 17:37:18 +0100

    corr

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 base = mysql.connector.connect(host='localhost',database='devinsa',user='root',password='devinsa!')
 
-curseur = base.cursos()
+curseur = base.cursor()
 
 curseur.execute("SHOW DATABASES")
 for x in mycursos:
