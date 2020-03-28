14a4b4b10c36a836a8ff75925ae1766fb67b4284
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-28 21:23:36 +0100

    corr

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -9,7 +9,7 @@ curseur.execute("SELECT yes_tfidf FROM app_answer WHERE item_id = 7336 and quest
 for x in curseur:
     print x
 
-curseur.execute("SELECT question_id FROM app_answer LIMIT 1")
+curseur.execute("SELECT title FROM app_answer WHERE id = 235")
 
 for x in curseur:
     print x
