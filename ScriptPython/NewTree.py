3b0a350972062d1d47efb18134f631d7d5085a6d
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-28 17:33:33 +0100

    new tree

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -2,4 +2,8 @@ import mysql.connector
 
 base = mysql.connector.connect(host='localhost',database='devinsa',user='root',password='devinsa!')
 
-print base
+curseur = base.cursos()
+
+curseur.execute("SHOW DATABASES")
+for x in mycursos:
+    print x
