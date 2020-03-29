4ab9ecf9f8061fae7224cd34d722cc10db86c9b4
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 14:19:37 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -4,12 +4,8 @@ base = mysql.connector.connect(host='localhost',database='devinsa',user='root',p
 
 curseur = base.cursor()
 
-curseur.execute("SELECT no_tfidf FROM app_answer WHERE item_id = 7336 and question_id = 235")
+curseur.execute("SELECT no_tfidf FROM app_answer WHERE item_id = (SELECT id FROM app_item WHERE name = 123)")
 
 for x in curseur:
     print x
 
-curseur.execute("SELECT title FROM app_question WHERE id = 235")
-
-for x in curseur:
-    print x
