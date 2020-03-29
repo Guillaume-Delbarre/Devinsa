0cbe22c97616b6d4b78fdb921d708ffd9ded111a
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 14:37:04 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -2,14 +2,14 @@ import mysql.connector
 
 base = mysql.connector.connect(host='localhost',database='devinsa',user='root',password='devinsa!')
 
+curseur = base.cursor()
 
-def extrait_itemID(base):
+def extrait_itemID(curseur):
     res = []
-    curseur = base.cursor()
     curseur.execute("SELECT id FROM app_item")
     for (x,) in curseur:
         res.append(x)
     return res
 
-print extrait_itemID(base)
+    
     
