7b264880c4e0545f078ce8fd562aea0f446054d2
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 14:22:01 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -4,7 +4,7 @@ base = mysql.connector.connect(host='localhost',database='devinsa',user='root',p
 
 curseur = base.cursor()
 
-curseur.execute("SELECT yes_count FROM app_answer WHERE item_id = (SELECT id FROM app_item WHERE name = 123) and question_id = (SELECT question_id FROM app_tree LIMIT 1")
+curseur.execute("SELECT yes_count FROM app_answer WHERE item_id = (SELECT id FROM app_item WHERE name = 123) and question_id = (SELECT question_id FROM app_tree LIMIT 1)")
 
 for x in curseur:
     print x
