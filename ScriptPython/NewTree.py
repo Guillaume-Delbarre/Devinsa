26ebde5d1b607e084e1ad4d53bd10184f4257681
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 14:56:21 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -18,7 +18,7 @@ def getfils(cursor,parent_id):
         res.append(x)
     return res
 
-curseur.execute("SELECT yes_count FROM app_answer WHERE item_id = 65 and question_id = (SELECT question_id FROM app_tree WHERE id = 27)")
+curseur.execute("SELECT yes_count FROM app_answer WHERE item_id = 65 and question_id = (SELECT question_id FROM app_tree WHERE depth = 0)")
 
 """curseur.execute("SELECT id FROM app_tree LIMIT 10")"""
 
