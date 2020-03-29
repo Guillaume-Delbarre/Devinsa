2085c933df58bcd09f518321af694c9febf29d9f
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 14:34:25 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -2,13 +2,14 @@ import mysql.connector
 
 base = mysql.connector.connect(host='localhost',database='devinsa',user='root',password='devinsa!')
 
-curseur = base.cursor()
 
-x = 235
+def extrait_itemID(base):
+    res = []
+    curseur = base.cursor()
+    curseur.execute("SELECT id FROM app_item")
+    for (x,) in curseur:
+        res.add(x)
+    return res
 
-curseur.execute("SELECT id FROM app_answer WHERE question_id ="+str(x)+" LIMIT 1")
-
-for (x,) in curseur:
-    print x
-    print type(x)
-
+print extrait_itemID(base)
+    
