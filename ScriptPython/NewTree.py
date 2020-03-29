0520e1297669bc09c3ef9d326a801efd54376d99
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 15:13:54 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -24,13 +24,22 @@ def compterPerso(cursor,liste_item_id,app_tree_id,choice):
     res = recopierTableau(liste_item_id)
     yes_count = 0
     no_count = 0
-    for i in range(len(res)):
+    rapport = 1
+    for i in range(len(liste_item_id)):
         cursor.execute("SELECT yes_count FROM app_answer WHERE item_id ="+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id ="+str(app_tree_id))
         for (x,) in cursor:
             yes_count = x
         cursor.execute("SELECT no_count FROM app_answer WHERE item_id ="+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id ="+str(app_tree_id))
         for (x,) in cursor:
             no_count = x
+        if choice = "o":
+            rapport = yes_count/no_count
+        if choice = "n":
+            rapport = no_count/yes_count
+        if rapport<0.75:
+            res.remove(liste_item_id[i])
+    return res
+                
         
 
 def recopierTableau(tableau):
@@ -39,13 +48,7 @@ def recopierTableau(tableau):
         res.append(tableau[i])
     return i
 
-curseur.execute("SELECT choice FROM app_tree LIMIT 5")
-
-for (x,) in curseur:
-        print x
-        if x == 'o':
-            print "yes"
-        print type(x)
+print compterPerso(curseur,extrait_itemID(curseur),1,"o")
 
 
     
