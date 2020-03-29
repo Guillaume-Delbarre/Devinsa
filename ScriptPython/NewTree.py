b25cdddf290b1947f1cfdc18544c3a4ef23e19c4
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 14:44:15 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -4,12 +4,19 @@ base = mysql.connector.connect(host='localhost',database='devinsa',user='root',p
 
 curseur = base.cursor()
 
-def extrait_itemID(curseur):
+def extrait_itemID(cursor):
     res = []
-    curseur.execute("SELECT id FROM app_item")
+    cursor.execute("SELECT id FROM app_item")
     for (x,) in curseur:
         res.append(x)
     return res
 
-print extrait_itemID(curseur)
+def getfils(cursor,parent_id):
+    res = []
+    cursor.execute("SELECT id FROM app_tree WHERE parent_id ="+parent_id)
+    for (x,) in cursor:
+        res.append(x)
+    return res
+
+print getfils(curseur,"149235")
     
