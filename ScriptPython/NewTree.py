3d37580f3195e3e561e4cddf1c2275e37e48fdd6
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 15:27:53 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -26,10 +26,10 @@ def compterPerso(cursor,liste_item_id,app_tree_id,choice):
     no_count = 0
     rapport = 1
     for i in range(len(liste_item_id)):
-        cursor.execute("SELECT yes_count FROM app_answer WHERE item_id = "+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id = "+str(app_tree_id))
+        cursor.execute("SELECT yes_count FROM app_answer WHERE item_id = "+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id = "+str(app_tree_id)+")")
         for (x,) in cursor:
             yes_count = x
-        cursor.execute("SELECT no_count FROM app_answer WHERE item_id ="+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id ="+str(app_tree_id))
+        cursor.execute("SELECT no_count FROM app_answer WHERE item_id ="+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id ="+str(app_tree_id)+")")
         for (x,) in cursor:
             no_count = x
         if choice == "o":
@@ -48,9 +48,7 @@ def recopierTableau(tableau):
         res.append(tableau[i])
     return i
 
-curseur.execute("SELECT yes_count FROM app_answer WHERE item_id = 65 and question_id = (SELECT question_id FROM app_tree WHERE id = 1")
-for (x,) in curseur:
-    print x
+print compterPerso(curseur,extrait_itemID(curseur),1,"o")
 
 
     
