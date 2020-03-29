322a7f87fe06827b9f559f2f195afdced7b9f3dd
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-29 15:07:20 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -18,9 +18,33 @@ def getfils(cursor,parent_id):
         res.append(x)
     return res
 
-curseur.execute("SELECT yes_count FROM app_answer WHERE item_id = 65 and question_id = (SELECT question_id FROM app_tree WHERE depth = 1)")
 
-for (x,) in curseur:
-    print x
+
+def compterPerso(cursor,liste_item_id,app_tree_id,choice):
+    res = recopierTableau(liste_item_id)
+    yes_count = 0
+    no_count = 0
+    for i in range(len(res)):
+        cursor.execute("SELECT yes_count FROM app_answer WHERE item_id ="+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id ="+str(app_tree_id))
+        for (x,) in cursor:
+            yes_count = x
+        cursor.execute("SELECT no_count FROM app_answer WHERE item_id ="+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id ="+str(app_tree_id))
+        for (x,) in cursor:
+            no_count = x
+        
+
+def recopierTableau(tableau):
+    res = []
+    for i in range(len(tableau)):
+        res.append(tableau[i])
+    return i
+
+curseur.execute("SELECT choice FROM app_tree LIMIT 1")
+
+for (x,) in cursor:
+        print x
+        print type(x)
+
+
     
     
