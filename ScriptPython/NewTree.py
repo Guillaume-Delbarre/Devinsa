4569ff9788a7a377eb4b180701ae4c233dda8cdc
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-30 17:35:36 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -13,58 +13,53 @@ base = mysql.connector.connect(host='localhost',database='devinsa',user='root',p
 
 curseur = base.cursor()
 
-def extrait_item(cursor):
+def extrait_app_item(cursor):
     res = []
     cursor.execute("SELECT name,id FROM app_item")
     for (x,y) in curseur:
         res.append([x,y])
     return res
 
-def extrait_answer(cursor):
+def extrait_app_answer(cursor):
     res = []
     cursor.execute("SELECT question_id,item_id,yes_count,no_count,yes_tfidf,no_tfidf FROM app_answer")
     for (a,b,c,d,e,f) in cursor:
         res.append([a,b,c,d,e,f])
     return res
 
-def extrait_tree(cursor):
+def extrait_app_tree(cursor):
     res = []
     cursor.execute("SELECT id,parent_id,choice,question_id FROM app_tree")
     for (a,b,c,d) in curseur:
         res.append([a,b,c,d])
     return res
 
-def extrait_question(curseur):
+def extrait_app_question(cursor):
     res = []
-    curseur.execute("SELECT id,title FROM app_question")
-    for (a,b) in curseur:
+    cursor.execute("SELECT id,title FROM app_question")
+    for (a,b) in cursor:
         res.append([a,b])
     return res
 
-def getfils(cursor,parent_id):
+def getfils(parent_id,app_tree):
     res = []
-    cursor.execute("SELECT id FROM app_tree WHERE parent_id ="+str(parent_id)+" and choice = 'o' ")
-    for (x,) in cursor:
-        res.append(x)
-    cursor.execute("SELECT id FROM app_tree WHERE parent_id ="+str(parent_id)+" and choice = 'n' ")
-    for (x,) in cursor:
-        res.append(x)
+    for i in range(len(app_tree)):
+        if app_tree[i][1]==parent_id:
+            res.append(app_tree[i][0])
     return res
 
 
 
-def compterPerso(cursor,liste_item_id,app_tree_id,choice):
+def compterPerso(app_item,app_answer,choice):
     res = recopierTableau(liste_item_id)
     yes_count = 0
     no_count = 0
     rapport = 1
-    for i in range(len(liste_item_id)):
-        cursor.execute("SELECT yes_count FROM app_answer WHERE item_id = "+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id = "+str(app_tree_id)+")")
-        for (x,) in cursor:
-            yes_count = x
-        cursor.execute("SELECT no_count FROM app_answer WHERE item_id ="+str(liste_item_id[i])+" and question_id = (SELECT question_id FROM app_tree WHERE id ="+str(app_tree_id)+")")
-        for (x,) in cursor:
-            no_count = x
+    for i in range(len(app_item)):
+        for j in range(len(app_answer)):
+            if app_item[i][1]==app_answer[j][1]:
+                yes_count = app_answer[j][2]
+                no_count = app_answer[j][3]
         if choice == "o" and no_count !=0:
             rapport = yes_count/no_count
         elif choice == "n" and yes_count!=0:
@@ -82,25 +77,24 @@ def recopierTableau(tableau):
         res.append(tableau[i])
     return res
 
-def median(cursor,liste_item_id,liste_question_id):
+def median(app_item,app_answer):
     med = []
     summ_yes = 0
     summ_no = 0
-    for question in range(len(liste_question_id)):
-        for item in range(len(liste_item_id)):
-            cursor.execute("SELECT yes_tfidf,no_tfidf FROM app_answer WHERE item_id = "+str(liste_item_id[item])+" and question_id = "+str(liste_question_id[question]))
-            for (x,y) in cursor:
-                summ_yes += x
-                summ_no += y
-        summ_yes = summ_yes/(len(liste_item_id))
+    for question in range(len(app_answer)):
+        for item in range(len(app_item)):
+            if app_answer[question][1]==app_item[item][1]:
+                summ_yes += app_answer[question][4] 
+                summ_no += app_answer[question][5]
+        summ_yes = summ_yes/(len(app_item))
         med.append(summ_yes)
-        summ_no = summ_no/(len(liste_item_id))
+        summ_no = summ_no/(len(app_item))
         med.append(summ_no)
         summ_yes = 0
         summ_no = 0
     return med
         
-a = extrait_question(curseur)
+a = median(extrait_app_item(curseur),extrait_app_answer(curseur))
 print a
 
 
