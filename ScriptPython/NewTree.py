75559776d88309f14665ea4400c2213e2bec1fa2
Ougo <hugo.martineau@insa-rennes.fr>
2020-03-31 20:53:17 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -148,18 +148,15 @@ def elaguer_app_tree(app_tree,question,res):
     if len(fils)==0:
         return
     elif len(fils) == 3:
-        ingrat = 0
         aux = recopierMatrice(app_tree)
         for i in range(len(fils)):
             aux.remove(fils[i])
         for i in range(len(fils)):
             if fils[i][2] == 'p':
-                ingrat = fils[i]
                 fils.remove(fils[i])
         if ingrat==0:
             print("Error\n")
             return
-        aux = aux_elaguer_app_tree(aux,ingrat,[])
         elaguer_app_tree(aux,fils[0],res)
         elaguer_app_tree(aux,fils[1],res)
         return res
