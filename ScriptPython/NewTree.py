87e6d5c74c123b397f76e55914c8b68a653abc07
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-01 20:11:58 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -162,18 +162,17 @@ def elaguer_app_tree(app_tree,question,res):
 def creation_matrice_perso(app_answer,app_item,liste_questions):
     res = [[None]*(2*len(liste_questions)-1)]*(len(app_item)+1)
     liste_questions = modifier_liste_questions(liste_questions)
-    print(liste_questions)
     for i in range(1,len(liste_questions)):
         res[0][i] = liste_questions[i]
-        print res[0][i][0]
+    print res
     for i in range(len(app_item)):
         res[i+1][0] = app_item[i][1]
         for j in range(1,len(res[0]),2):
-            id_question = res[0][j]
+            id_question = res[0][j][0]
             for k in range(len(app_answer)):
                 if app_answer[k][1] == app_item[i][0] and app_answer[k][0] == id_question:
-                    res[i][j] = app_answer[k][4]
-                    res[i][j+1] = app_answer[k][5]
+                    res[i+1][j] = app_answer[k][4]
+                    res[i+1][j+1] = app_answer[k][5]
     print [res[1][0],res[1][1],res[1][2]]
     print [res[2][0],res[2][1],res[2][2]]
     print [res[3][0],res[3][1],res[3][2]]
