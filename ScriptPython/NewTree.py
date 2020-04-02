226c104a17cc453884829fe47f372419a36898a6
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-02 11:39:52 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -193,9 +193,8 @@ def init(curseur):
     app_question = extrait_app_question(curseur)
     app_tree = elaguer_app_tree(app_tree,app_tree[0],[])
     liste_questions = garder_questions_arbre(app_tree,app_question)
-    liste_questions = modifier_liste_questions(liste_questions)
-    print liste_questions[1][0]
     app_answer = garder_reponses_arbre(app_answer,liste_questions)
+    liste_questions = modifier_liste_questions(liste_questions)
     creation_matrice_perso(app_answer,app_item,liste_questions)
     
 
