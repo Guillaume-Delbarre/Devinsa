2d22be06ece7c0e2e7395ae7b54a0ea02fb7bac1
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-02 18:36:21 +0200

    new arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -223,7 +223,8 @@ def main(curseur):
     #Preparation de liste_questions pour creer une matrice tfidf_oui,non pour chaque (perso,question)
     liste_questions = modifier_liste_questions(liste_questions)
     matricePerso = creation_matrice_perso(app_answer,app_item,liste_questions)
-    file = "../Web/Arbre_Binaire/Treejavascript.js"
+    print matrcePerso[30][20]
+    """file = "../Web/Arbre_Binaire/Treejavascript.js"
     ecriture = open(file,"w",encoding="utf-8")
     ecriture.write("questionid_1 = {text: { name: '"+app_tree[0][4]+"' }, collapsed : true};\n")
     chart_config_init = "chart_config = [\n{container: '#basic-example',\nconnectors: { type: 'step' },\n node: { HTMLclass: 'nodeExample1' },\n animation: { nodeAnimation: "+'"'+"easeOutBounce"+'"'+", nodeSpeed: 700,connectorsAnimation: "+'"'+"bounce"+'"'+", connectorsSpeed: 700 }},\n questionid_1,"
@@ -232,7 +233,7 @@ def main(curseur):
     chart_config = chart_config[0:len(chart_config)-2]
     chart_config += "];"
     ecriture.write(chart_config)
-    ecriture.close
+    ecriture.close"""
     
 main(curseur)
 
