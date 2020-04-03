5e9bb907cf257f407916d6a52be789dfa4132631
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-03 11:19:06 +0200

    test3

--- Web/Arbre_Binaire/Treejavascript.js
+++ Web/Arbre_Binaire/Treejavascript.js
@@ -1,11 +1,10 @@
 questionid_1 = {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true};
-questionid_1 = {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true};
-questionid_2 = {parent: questionid_1,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il français ? Personnages restants : 1416 Personnage median : Samir Loussif '}, collapsed : true};
-questionid_3 = {parent: questionid_2,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 1409 Personnage median : Samir Loussif '}, collapsed : true};
-questionid_259552 = {parent: questionid_2,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il américain ? Personnages restants : 1364 Personnage median : Samir Loussif '}, collapsed : true};
-questionid_782121 = {parent: questionid_1,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il un personnage de jeu vidéo ? Personnages restants : 1722 Personnage median : Skelita Calaveras '}, collapsed : true};
-questionid_782122 = {parent: questionid_782121,text: { name: 'Choix : o', desc : 'Titre : Ton personnage porte-t-il des chaussures ? Personnages restants : 1718 Personnage median : Skelita Calaveras '}, collapsed : true};
-questionid_1044104 = {parent: questionid_782121,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il des cheveux ? Personnages restants : 1629 Personnage median : Skelita Calaveras '}, collapsed : true};
+questionid_2 = {parent: questionid_1,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il français ? Personnages restants : 466 Personnage median :Aelita Stones '}, collapsed : true};
+questionid_3 = {parent: questionid_2,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 53 Personnage median :Poséïdon '}, collapsed : true};
+questionid_259552 = {parent: questionid_2,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il américain ? Personnages restants : 7 Personnage median :Dalida (chanteuse franbçaise)'}, collapsed : true};
+questionid_782121 = {parent: questionid_1,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il un personnage de jeu vidéo ? Personnages restants : 772 Personnage median :f'}, collapsed : true};
+questionid_782122 = {parent: questionid_782121,text: { name: 'Choix : o', desc : 'Titre : Ton personnage porte-t-il des chaussures ? Personnages restants : 93 Personnage median :hayao miyazaki'}, collapsed : true};
+questionid_1044104 = {parent: questionid_782121,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il des cheveux ? Personnages restants : 6 Personnage median :Cheikh Ndoye'}, collapsed : true};
 chart_config = [
 {container: '#basic-example',
 connectors: { type: 'step' },
