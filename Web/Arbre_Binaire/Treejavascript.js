9bb20b9f3bbec803ac7a2e82af90b8b6a32bef73
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-04 16:00:41 +0200

    final profondeur 7

--- Web/Arbre_Binaire/Treejavascript.js
+++ Web/Arbre_Binaire/Treejavascript.js
@@ -1,10 +1,66 @@
 questionid_1 = {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true};
-questionid_2 = {parent: questionid_1,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il français ? Personnages restants : 772 Personnage median :f'}, collapsed : true};
-questionid_3 = {parent: questionid_2,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 423 Personnage median :Booba '}, collapsed : true};
-questionid_259552 = {parent: questionid_2,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il américain ? Personnages restants : 352 Personnage median :Adam Ondra'}, collapsed : true};
-questionid_782121 = {parent: questionid_1,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il un personnage de jeu vidéo ? Personnages restants : 466 Personnage median :Aelita Stones '}, collapsed : true};
-questionid_782122 = {parent: questionid_782121,text: { name: 'Choix : o', desc : 'Titre : Ton personnage porte-t-il des chaussures ? Personnages restants : 119 Personnage median :Arthas (Roi-Liche de Warcraft)'}, collapsed : true};
-questionid_1044104 = {parent: questionid_782121,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il des cheveux ? Personnages restants : 289 Personnage median :Aelita Stones '}, collapsed : true};
+questionid_2 = {parent: questionid_1,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il français ? Personnages restants : 772 Personnage median :PPDA'}, collapsed : true};
+questionid_3 = {parent: questionid_2,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 423 Personnage median :david halliday'}, collapsed : true};
+questionid_4 = {parent: questionid_3,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il un écrivain ? Personnages restants : 100 Personnage median :Henri-Léon Lebesgue (mathématicien)'}, collapsed : true};
+questionid_5 = {parent: questionid_4,text: { name: 'Choix : o', desc : 'Titre : Ton personnage a-t-il un prénom hors du commun ? Personnages restants : 21 Personnage median :André Malraux'}, collapsed : true};
+questionid_6 = {parent: questionid_5,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il policier ou détective ? Personnages restants : 6 Personnage median :Aimé Césaire (écrivain)'}, collapsed : true};
+questionid_8884 = {parent: questionid_5,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il une femme ? Personnages restants : 16 Personnage median :Albert Camus'}, collapsed : true};
+questionid_27267 = {parent: questionid_4,text: { name: 'Choix : n', desc : 'Titre : Ton personnage fait-il de la politique ? Personnages restants : 63 Personnage median :Henri-Léon Lebesgue (mathématicien)'}, collapsed : true};
+questionid_27268 = {parent: questionid_27267,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il devenu connu grâce à Internet ? Personnages restants : 25 Personnage median :Eugène Livet (éducateur français)'}, collapsed : true};
+questionid_37109 = {parent: questionid_27267,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il devenu connu grâce à Internet ? Personnages restants : 39 Personnage median :Henri-Léon Lebesgue (mathématicien)'}, collapsed : true};
+questionid_85898 = {parent: questionid_3,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il un rapport avec le milieu du sport ? Personnages restants : 323 Personnage median :david halliday'}, collapsed : true};
+questionid_85899 = {parent: questionid_85898,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il un footballeur ? Personnages restants : 88 Personnage median :David Belle (sportif acteur français)'}, collapsed : true};
+questionid_85900 = {parent: questionid_85899,text: { name: 'Choix : o', desc : 'Titre : Ton personnage a-t-il plus de 35 ans ? Personnages restants : 22 Personnage median :Raphaël Varane'}, collapsed : true};
+questionid_94382 = {parent: questionid_85899,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il plus de 50 ans ? Personnages restants : 53 Personnage median :David Belle (sportif acteur français)'}, collapsed : true};
+questionid_112048 = {parent: questionid_85898,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il devenu connu grâce à Internet ? Personnages restants : 235 Personnage median :david halliday'}, collapsed : true};
+questionid_112049 = {parent: questionid_112048,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il un garçon ? Personnages restants : 51 Personnage median :Torlk'}, collapsed : true};
+questionid_121854 = {parent: questionid_112048,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il célèbre  ? Personnages restants : 151 Personnage median :Dominique Strauss-Kahn (dit DSK, homme politique)'}, collapsed : true};
+questionid_259552 = {parent: questionid_2,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il américain ? Personnages restants : 352 Personnage median :Keith Urban (chanteur)'}, collapsed : true};
+questionid_259553 = {parent: questionid_259552,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il acteur/actrice ? Personnages restants : 121 Personnage median :Marion Jones (athlète américaine)'}, collapsed : true};
+questionid_259554 = {parent: questionid_259553,text: { name: 'Choix : o', desc : 'Titre : Ton personnage a-t-il plus de 35 ans ? Personnages restants : 40 Personnage median :Robert Downey Jr (acteur de iron man)'}, collapsed : true};
+questionid_259555 = {parent: questionid_259554,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 32 Personnage median :Mark Hamill (Acteur de Luke Skywalker)'}, collapsed : true};
+questionid_269009 = {parent: questionid_259554,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 7 Personnage median :Sasha Grey'}, collapsed : true};
+questionid_286441 = {parent: questionid_259553,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il musicien ? Personnages restants : 68 Personnage median :Tom Brady (joueur de foot américain)'}, collapsed : true};
+questionid_286442 = {parent: questionid_286441,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 18 Personnage median :Logic (rappeur)'}, collapsed : true};
+questionid_295617 = {parent: questionid_286441,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 34 Personnage median :Bill Clinton (42e président américain)'}, collapsed : true};
+questionid_344112 = {parent: questionid_259552,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il anglais ? Personnages restants : 209 Personnage median :Keith Urban (chanteur)'}, collapsed : true};
+questionid_344113 = {parent: questionid_344112,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il acteur/actrice ? Personnages restants : 41 Personnage median :Rob Hopkins (enseignant ern permaculture brittanique)'}, collapsed : true};
+questionid_344114 = {parent: questionid_344113,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 7 Personnage median :Paul Taylor'}, collapsed : true};
+questionid_352461 = {parent: questionid_344113,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il japonais ? Personnages restants : 25 Personnage median :Rob Hopkins (enseignant ern permaculture brittanique)'}, collapsed : true};
+questionid_372089 = {parent: questionid_344112,text: { name: 'Choix : n', desc : 'Titre : La célébrité de ton personnage est-elle en rapport avec le monde du football ? Personnages restants : 154 Personnage median :Joseph Goebbels (dirigeant nazi)'}, collapsed : true};
+questionid_372090 = {parent: questionid_372089,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 13 Personnage median :Manuel Neuer (gardien de foot allemand)'}, collapsed : true};
+questionid_381931 = {parent: questionid_372089,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il décédé ? Personnages restants : 67 Personnage median :Caballero (rappeur)'}, collapsed : true};
+questionid_782121 = {parent: questionid_1,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il un personnage de jeu vidéo ? Personnages restants : 466 Personnage median :La fée clochette '}, collapsed : true};
+questionid_782122 = {parent: questionid_782121,text: { name: 'Choix : o', desc : 'Titre : Ton personnage porte-t-il des chaussures ? Personnages restants : 119 Personnage median :Shulk'}, collapsed : true};
+questionid_782123 = {parent: questionid_782122,text: { name: 'Choix : o', desc : 'Titre : Ton personnage possède-t-il des armes ? Personnages restants : 85 Personnage median :Shulk'}, collapsed : true};
+questionid_782124 = {parent: questionid_782123,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il masculin ? Personnages restants : 40 Personnage median :Richter Belmont (Castlevania)'}, collapsed : true};
+questionid_782125 = {parent: questionid_782124,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il méchant ?  Personnages restants : 24 Personnage median :Richter Belmont (Castlevania)'}, collapsed : true};
+questionid_791966 = {parent: questionid_782124,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il méchant ?  Personnages restants : 3 Personnage median :Riven (jpersonnage de jeu vidéo)'}, collapsed : true};
+questionid_811648 = {parent: questionid_782123,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il la peau blanche ? Personnages restants : 25 Personnage median :Jill (Va-11 Hall-a)'}, collapsed : true};
+questionid_811649 = {parent: questionid_811648,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il méchant ?  Personnages restants : 6 Personnage median :Adibou'}, collapsed : true};
+questionid_821436 = {parent: questionid_811648,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il masculin ? Personnages restants : 4 Personnage median :Le grand schtroumpf'}, collapsed : true};
+questionid_869940 = {parent: questionid_782122,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il méchant ?  Personnages restants : 28 Personnage median :cars (personnage voiture dessin animé)'}, collapsed : true};
+questionid_869941 = {parent: questionid_869940,text: { name: 'Choix : o', desc : 'Titre : Ton personnage a-t-il la peau blanche ? Personnages restants : 12 Personnage median :riki'}, collapsed : true};
+questionid_869942 = {parent: questionid_869941,text: { name: 'Choix : o', desc : 'Titre : Ton personnage a-t-il un prénom hors du commun ? Personnages restants : 2 Personnage median :Monokuma (ours mécanique manga japonais Danganronpa)'}, collapsed : true};
+questionid_878601 = {parent: questionid_869941,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il des yeux ? Personnages restants : 4 Personnage median :Un zombie '}, collapsed : true};
+questionid_897020 = {parent: questionid_869940,text: { name: 'Choix : n', desc : 'Titre : Est-ce que ton personnage est un animal ? Personnages restants : 15 Personnage median :cars (personnage voiture dessin animé)'}, collapsed : true};
+questionid_897021 = {parent: questionid_897020,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il masculin ? Personnages restants : 6 Personnage median :Simba'}, collapsed : true};
+questionid_906727 = {parent: questionid_897020,text: { name: 'Choix : n', desc : 'Titre : Ton personnage possède-t-il des armes ? Personnages restants : 7 Personnage median :Tachibana(manga japonais)'}, collapsed : true};
+questionid_1044104 = {parent: questionid_782121,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il des cheveux ? Personnages restants : 289 Personnage median :La fée clochette '}, collapsed : true};
+questionid_1044105 = {parent: questionid_1044104,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il un personnage de série télévisée ? Personnages restants : 202 Personnage median :Ashitaka (personnaage film d'animation)'}, collapsed : true};
+questionid_1044106 = {parent: questionid_1044105,text: { name: 'Choix : o', desc : 'Titre : Ton personnage est-il policier ou détective ? Personnages restants : 79 Personnage median :Archie (personnage de Riverdale)'}, collapsed : true};
+questionid_1044107 = {parent: questionid_1044106,text: { name: 'Choix : o', desc : 'Titre : Ton personnage possède-t-il des armes ? Personnages restants : 7 Personnage median :Dexter Morgan'}, collapsed : true};
+questionid_1053753 = {parent: questionid_1044106,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il la peau blanche ? Personnages restants : 67 Personnage median :Archie (personnage de Riverdale)'}, collapsed : true};
+questionid_1073435 = {parent: questionid_1044105,text: { name: 'Choix : n', desc : 'Titre : Ton personnage possède-t-il des armes ? Personnages restants : 126 Personnage median :Ashitaka (personnaage film d'animation)'}, collapsed : true};
+questionid_1073436 = {parent: questionid_1073435,text: { name: 'Choix : o', desc : 'Titre : Ton personnage a-t-il un prénom hors du commun ? Personnages restants : 51 Personnage median :Ashitaka (personnaage film d'animation)'}, collapsed : true};
+questionid_1083217 = {parent: questionid_1073435,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il un prénom hors du commun ? Personnages restants : 39 Personnage median :La fée clochette '}, collapsed : true};
+questionid_1132315 = {parent: questionid_1044104,text: { name: 'Choix : n', desc : 'Titre : Ton personnage est-il méchant ?  Personnages restants : 68 Personnage median :E.T. (alien)'}, collapsed : true};
+questionid_1132316 = {parent: questionid_1132315,text: { name: 'Choix : o', desc : 'Titre : Est-ce que ton personnage est un animal ? Personnages restants : 12 Personnage median :Voldemort (personnage d'Harry Potter)'}, collapsed : true};
+questionid_1132317 = {parent: questionid_1132316,text: { name: 'Choix : o', desc : 'Titre : Ton personnage a-t-il la peau blanche ? Personnages restants : 2 Personnage median :un dragon'}, collapsed : true};
+questionid_1142077 = {parent: questionid_1132316,text: { name: 'Choix : n', desc : 'Titre : Ton personnage a-t-il la peau blanche ? Personnages restants : 10 Personnage median :Voldemort (personnage d'Harry Potter)'}, collapsed : true};
+questionid_1161678 = {parent: questionid_1132315,text: { name: 'Choix : n', desc : 'Titre : Ton personnage porte-t-il des chaussures ? Personnages restants : 47 Personnage median :E.T. (alien)'}, collapsed : true};
+questionid_1161679 = {parent: questionid_1161678,text: { name: 'Choix : o', desc : 'Titre : Ton personnage possède-t-il des armes ? Personnages restants : 17 Personnage median :Dingo (ami de mickey)'}, collapsed : true};
+questionid_1171520 = {parent: questionid_1161678,text: { name: 'Choix : n', desc : 'Titre : Est-ce que ton personnage est un animal ? Personnages restants : 31 Personnage median :E.T. (alien)'}, collapsed : true};
 chart_config = [
 {container: '#basic-example',
 connectors: { type: 'step' },
@@ -13,7 +69,63 @@ connectors: { type: 'step' },
 questionid_1,
 questionid_2,
 questionid_3,
+questionid_4,
+questionid_5,
+questionid_6,
+questionid_8884,
+questionid_27267,
+questionid_27268,
+questionid_37109,
+questionid_85898,
+questionid_85899,
+questionid_85900,
+questionid_94382,
+questionid_112048,
+questionid_112049,
+questionid_121854,
 questionid_259552,
+questionid_259553,
+questionid_259554,
+questionid_259555,
+questionid_269009,
+questionid_286441,
+questionid_286442,
+questionid_295617,
+questionid_344112,
+questionid_344113,
+questionid_344114,
+questionid_352461,
+questionid_372089,
+questionid_372090,
+questionid_381931,
 questionid_782121,
 questionid_782122,
-questionid_1044104];
+questionid_782123,
+questionid_782124,
+questionid_782125,
+questionid_791966,
+questionid_811648,
+questionid_811649,
+questionid_821436,
+questionid_869940,
+questionid_869941,
+questionid_869942,
+questionid_878601,
+questionid_897020,
+questionid_897021,
+questionid_906727,
+questionid_1044104,
+questionid_1044105,
+questionid_1044106,
+questionid_1044107,
+questionid_1053753,
+questionid_1073435,
+questionid_1073436,
+questionid_1083217,
+questionid_1132315,
+questionid_1132316,
+questionid_1132317,
+questionid_1142077,
+questionid_1161678,
+questionid_1161679,
+questionid_1171520];
