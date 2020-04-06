0ec8efe786e57e68ee69289dfbc67276074e7ff1
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2020-04-06 10:14:17 +0200

    netoyage

---
+++ Web/graph/cluster.js
@@ -0,0 +1,45 @@
+(function (d3) {
+    'use strict';
+
+    var couleur = d3.scaleOrdinal(d3.schemeCategory10)
+        .domain(["Groupe 0","Groupe 1","Groupe 2","Groupe 3","Groupe 4","Groupe 5","Groupe 6","Groupe 7","Groupe 8","Groupe 9"])
+
+    var tabulate = function (data,columns) {
+        var table = d3.select('body').append('table')
+          var thead = table.append('thead')
+          var tbody = table.append('tbody')
+      
+          thead.append('tr')
+            .selectAll('th')
+              .data(columns)
+              .enter()
+            .append('th')
+              .style('color', function(d) {return couleur(d)})
+              .text(function (d) { return d })
+      
+          var rows = tbody.selectAll('tr')
+              .data(data)
+              .enter()
+            .append('tr')
+      
+          var cells = rows.selectAll('td')
+              .data(function(row) {
+                  return columns.map(function (column) {
+                      return { column: column, value: row[column] }
+                })
+            })
+            .enter()
+          .append('td')
+            .text(function (d) { return d.value })
+      
+        return table;
+      }
+
+    d3.csv('https://raw.githubusercontent.com/Guillaume-Delbarre/Devinsa/master/Donnees/questionDiffCluster.csv')
+        .then(data => {
+            
+            console.log(data[0]['Q0'])
+            console.log(data[0])
+            
+        });
+}(d3));
