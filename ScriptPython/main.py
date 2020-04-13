2188a34dfbb5efc5f99307d2f9b88c107b61dcbd
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2020-04-13 10:48:04 +0200

    Calcul temps

--- ScriptPython/main.py
+++ ScriptPython/main.py
@@ -11,12 +11,19 @@ from PCA import to2D
 from ScriptPCA import PCA
 from questionsCaracteristiques import printQuestionCarac
 
+import time
+
 def main(numberOfClusters=5, nbQuestion=10):
+    temps = time.time()
     miseEnPage()
+    print("Temps mis en page : %s secondes ---" % (time.time() - temps))
     #kmeansAlgo(numberOfClusters)
     classHierarchique(numberOfClusters)
+    print("Temps Classification hierarchique : %s secondes ---" % (time.time() - temps))
     printQuestionCarac(numberOfClusters,nbQuestion)
+    print("Temps print question carac : %s secondes ---" % (time.time() - temps))
     to2D()
+    print("Temps to 2 D : %s secondes ---" % (time.time() - temps))
         
 if __name__ == '__main__':
     main()
