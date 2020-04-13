57c3a86277c399f7e878709ba22b5f7e17e8e9de
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-13 20:10:17 +0200

    avancé arbre

--- ScriptPython/NewTree.py
+++ ScriptPython/NewTree.py
@@ -30,7 +30,7 @@ def extrait_app_answer(cursor):
 
 def extrait_app_tree(cursor):
     res = []
-    cursor.execute("SELECT app_tree.id,parent_id,choice,question_id,title FROM app_tree,app_question WHERE app_tree.question_id = app_question.id and app_tree.choice<>'p' and depth<7")
+    cursor.execute("SELECT app_tree.id,parent_id,choice,question_id,title FROM app_tree,app_question WHERE app_tree.question_id = app_question.id and app_tree.choice<>'p' and depth<9")
     for (a,b,c,d,e) in curseur:
         res.append([a,b,c,d,e])
     return res
