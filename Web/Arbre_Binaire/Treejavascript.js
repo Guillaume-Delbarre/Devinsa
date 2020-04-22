bbf9ee9e44c464e43bf9aac9f7c2210f4ed974f3
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-22 15:09:38 +0200

    test

--- Web/Arbre_Binaire/Treejavascript.js
+++ Web/Arbre_Binaire/Treejavascript.js
@@ -1,18 +1,18 @@
 questionid_1 = {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true};
-questionid_2 = {parent: questionid_1, HTMLclass :'light-green', text: { name: ' Personnages restants : 772 Personnage median :Passe-partout,Alex Keryhuel (étudiant INSA),Aya Nakamura (chanteuse)', desc : 'Prochaine question : Ton personnage est-il français ?'}, collapsed : true};
-questionid_3 = {parent: questionid_2, HTMLclass :'light-green', text: { name: ' Personnages restants : 423 Personnage median :Paul Queva (étudiant INSA),Aya Nakamura (chanteuse),Alex Keryhuel (étudiant INSA)', desc : 'Prochaine question : Ton personnage est-il décédé ?'}, collapsed : true};
-questionid_4 = {parent: questionid_3, HTMLclass :'light-green', text: { name: ' Personnages restants : 100 Personnage median :Caroline Aigle (pilote de chasse),Camille Claudel,Augustin Louis Cauchy (mathématicien français)', desc : 'Prochaine question : Ton personnage est-il un écrivain ?'}, collapsed : true};
-questionid_85898 = {parent: questionid_3, HTMLclass :'light-red', text: { name: ' Personnages restants : 323 Personnage median :Alkpote (rappeur français),Aya Nakamura (chanteuse),Alex Keryhuel (étudiant INSA)', desc : 'Prochaine question : Ton personnage a-t-il un rapport avec le milieu du sport ?'}, collapsed : true};
-questionid_259552 = {parent: questionid_2, HTMLclass :'light-red', text: { name: ' Personnages restants : 352 Personnage median :Didier Drogba (footballeur Ivoirien),Hideo Kojima (créateur japonais de jeux vidéos),Hubert Reeves', desc : 'Prochaine question : Ton personnage est-il américain ?'}, collapsed : true};
-questionid_259553 = {parent: questionid_259552, HTMLclass :'light-green', text: { name: ' Personnages restants : 121 Personnage median :Lana del Rey,Ariana Grande (chanteuse américaine),Lil Pump (rappeur américain)', desc : 'Prochaine question : Ton personnage est-il acteur/actrice ?'}, collapsed : true};
-questionid_344112 = {parent: questionid_259552, HTMLclass :'light-red', text: { name: ' Personnages restants : 209 Personnage median :Didier Drogba (footballeur Ivoirien),Hideo Kojima (créateur japonais de jeux vidéos),Hubert Reeves', desc : 'Prochaine question : Ton personnage est-il anglais ?'}, collapsed : true};
-questionid_782121 = {parent: questionid_1, HTMLclass :'light-red', text: { name: ' Personnages restants : 466 Personnage median :Brice de Nice,Ant-man (super héros Marvel),Malicia (X-men)', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'}, collapsed : true};
-questionid_782122 = {parent: questionid_782121, HTMLclass :'light-green', text: { name: ' Personnages restants : 119 Personnage median :Lee Sin (League Of Legends),Shulk,Chun-Li (jeu vidéo Steet-Fighter)', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'}, collapsed : true};
-questionid_782123 = {parent: questionid_782122, HTMLclass :'light-green', text: { name: ' Personnages restants : 85 Personnage median :Chun-Li (jeu vidéo Steet-Fighter),Lee Sin (League Of Legends),Richter Belmont (Castlevania)', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'}, collapsed : true};
-questionid_869940 = {parent: questionid_782122, HTMLclass :'light-red', text: { name: ' Personnages restants : 28 Personnage median :Ganymède (oiseau compagnon de Bastion, jeu vidéo Overwatch),Vel\'Koz (jeu: ligue of legend),cars (personnage voiture dessin animé)', desc : 'Prochaine question : Ton personnage est-il méchant ? '}, collapsed : true};
-questionid_1044104 = {parent: questionid_782121, HTMLclass :'light-red', text: { name: ' Personnages restants : 289 Personnage median :Brice de Nice,Ant-man (super héros Marvel),Malicia (X-men)', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'}, collapsed : true};
-questionid_1044105 = {parent: questionid_1044104, HTMLclass :'light-green', text: { name: ' Personnages restants : 202 Personnage median :Brice de Nice,Malicia (X-men),Ant-man (super héros Marvel)', desc : 'Prochaine question : Ton personnage est-il un personnage de série télévisée ?'}, collapsed : true};
-questionid_1132315 = {parent: questionid_1044104, HTMLclass :'light-red', text: { name: ' Personnages restants : 68 Personnage median :Le soleil des Télétubbies,Plutot,E.T. (alien)', desc : 'Prochaine question : Ton personnage est-il méchant ? '}, collapsed : true};
+questionid_2 = {parent: questionid_1, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_3 = {parent: questionid_2, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_4 = {parent: questionid_3, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_85898 = {parent: questionid_3, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_259552 = {parent: questionid_2, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_259553 = {parent: questionid_259552, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_344112 = {parent: questionid_259552, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_782121 = {parent: questionid_1, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_782122 = {parent: questionid_782121, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_782123 = {parent: questionid_782122, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_869940 = {parent: questionid_782122, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_1044104 = {parent: questionid_782121, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_1044105 = {parent: questionid_1044104, text: { name: ' Personnages restants : 0'}, collapsed : true};
+questionid_1132315 = {parent: questionid_1044104, text: { name: ' Personnages restants : 0'}, collapsed : true};
 chart_config = [
 {container: '#basic-example',
 connectors: { type: 'straight' },
