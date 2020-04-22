3d72b479548859917b7bfa3b02c60dee3bd84d2c
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-22 14:22:20 +0200

    l

--- ScriptPython/finalTree.py
+++ ScriptPython/finalTree.py
@@ -118,7 +118,7 @@ def elagagePerso(question,app_tree,matricePerso,ecriture):
             listeperso = proxi(median(matricePerso),matricePerso)
             perso_median = ""
             for i in range(len(listeperso)):
-                perso_median += listeperso[i]+","
+                perso_median += listeperso[i][1]+","
             perso_median = perso_median[:len(perso_median)-1]
             html = HTMLclass(question[2])
             ecriture.write("questionid_"+str(question[0])+" = {parent: questionid_"+str(question[1])+", HTMLclass :'"+html+"', text: { name: ' Personnages restants : "+str(len(matricePerso)-1)+" Personnage median :"+miseEnFormeText(perso_median)+"', desc : 'Prochaine question : "+miseEnFormeText(question[4])+"'}, collapsed : true};\n")
