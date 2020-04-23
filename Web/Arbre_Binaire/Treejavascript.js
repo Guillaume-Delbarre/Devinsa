eb4598d54a996b53fee552cde4d0cb38a5ea68ff
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-23 11:28:42 +0200

    mep

--- Web/Arbre_Binaire/Treejavascript.js
+++ Web/Arbre_Binaire/Treejavascript.js
@@ -5,27 +5,279 @@ connectors: { type: 'straight' },
 nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 772 Personnage median :f,Passe-partout,Alex Keryhuel (étudiant INSA)', desc : 'Prochaine question : Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: ' Personnages restants : 772 Personnage median :PPDA,Jean Luc Reichmann,Dj Snake (Dj)', desc : 'Prochaine question : Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 423 Personnage median :Booba ,Lorenzo,Paul Queva (étudiant INSA)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: ' Personnages restants : 423 Personnage median :Matthieu Chedid (musicien français dit M),Dominique Strauss-Kahn (dit DSK, homme politique),david halliday', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 100 Personnage median :Caroline Aigle (pilote de chasse),Henri-Léon Lebesgue (mathématicien),Camille Claudel', desc : 'Prochaine question : Ton personnage est-il un écrivain ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 21 Personnage median :André Malraux,Marcel Proust (ecrivain),Jules Verne (écrivain)', desc : 'Prochaine question : Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 6 Personnage median :123,123,Aimé Césaire (écrivain)', desc : 'Prochaine question : Ton personnage est-il policier ou détective ?'},HTMLclass :'light-green',collapsed : true, children : [
 ]
 }, 
  {
-text: { name: ' Personnages restants : 352 Personnage median :Alan Walker (DJ  britannico-norvégien),Banksy (artiste brittanique),Adam Ondra', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: ' Personnages restants : 16 Personnage median :Albert Camus,Jean de La Fontaine,André Malraux', desc : 'Prochaine question : Ton personnage est-il une femme ?'},HTMLclass :'light-red',collapsed : true, children : [
 ]
  } 
 ]
 }, 
  {
-text: { name: ' Personnages restants : 466 Personnage median :Andrew Wiggins (personnage livre),Alex Vaus (Personnage de serie TV:Orange is the new black),Aelita Stones ', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: ' Personnages restants : 63 Personnage median :Henri-Léon Lebesgue (mathématicien),Augustin Louis Cauchy (mathématicien français),Albert Jacquard (biologiste français)', desc : 'Prochaine question : Ton personnage fait-il de la politique ?'},HTMLclass :'light-red',collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 119 Personnage median :Bart,Bob le Bricoleur,Arthas (Roi-Liche de Warcraft)', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: ' Personnages restants : 25 Personnage median :Maréchal Pétain ,Raymond Poincarré (politicien français),Eugène Livet (éducateur français)', desc : 'Prochaine question : Ton personnage est-il devenu connu grâce à Internet ?'},HTMLclass :'light-green',collapsed : true, children : [
 ]
 }, 
  {
-text: { name: ' Personnages restants : 289 Personnage median :Alex Vaus (Personnage de serie TV:Orange is the new black),Andrew Wiggins (personnage livre),Aelita Stones ', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: ' Personnages restants : 39 Personnage median :Augustin Louis Cauchy (mathématicien français),Albert Jacquard (biologiste français),Henri-Léon Lebesgue (mathématicien)', desc : 'Prochaine question : Ton personnage est-il devenu connu grâce à Internet ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 323 Personnage median :david halliday,Matthieu Chedid (musicien français dit M),Dj Snake (Dj)', desc : 'Prochaine question : Ton personnage a-t-il un rapport avec le milieu du sport ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 88 Personnage median :David Belle (sportif acteur français),Paul Pogba (footballeur),Antoine Albeau (sportif (planche à voile) francais le plus titré ', desc : 'Prochaine question : Ton personnage est-il un footballeur ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 22 Personnage median :Alexandre Lacazette,Raphaël Varane,Khaled Mesbah (footballer Algerien)', desc : 'Prochaine question : Ton personnage a-t-il plus de 35 ans ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 53 Personnage median :David Belle (sportif acteur français),Aurélie Dupont (directrice opera Paris),Sébastien Loeb (pilote de rallye français)', desc : 'Prochaine question : Ton personnage a-t-il plus de 50 ans ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 235 Personnage median :Matthieu Chedid (musicien français dit M),Dominique Strauss-Kahn (dit DSK, homme politique),david halliday', desc : 'Prochaine question : Ton personnage est-il devenu connu grâce à Internet ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 51 Personnage median :Torlk,Laink,DavidLaFargePokémon (youtubeur)', desc : 'Prochaine question : Ton personnage est-il un garçon ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 151 Personnage median :Jean Luc Reichmann,Dominique Strauss-Kahn (dit DSK, homme politique),Jean-Pierre Pernault', desc : 'Prochaine question : Ton personnage est-il célèbre  ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 352 Personnage median :Mohammed 6,Keith Urban (chanteur),Patrice Talon', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 121 Personnage median :Tom Brady (joueur de foot américain),Travis Pastrana (pilote américain),Marion Jones (athlète américaine)', desc : 'Prochaine question : Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 40 Personnage median :Morgan Freeman (acteur américain),Mark Hamill (Acteur de Luke Skywalker),Robert Downey Jr (acteur de iron man)', desc : 'Prochaine question : Ton personnage a-t-il plus de 35 ans ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 32 Personnage median :Tom Hanks (acteur américain),Mark Hamill (Acteur de Luke Skywalker),Robert Downey Jr (acteur de iron man)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 7 Personnage median :Emilia Clarke (acteur de Game of Thrones),Sasha Grey,Taylor Swift (chanteuse american)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 68 Personnage median :David Copperfield (Magicien),Tom Brady (joueur de foot américain),Marion Jones (athlète américaine)', desc : 'Prochaine question : Ton personnage est-il musicien ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 18 Personnage median :Ariana Grande (chanteuse américaine),Tommy Tallarico,Logic (rappeur)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 34 Personnage median :Marion Jones (athlète américaine),Bill Clinton (42e président américain),Travis Pastrana (pilote américain)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 209 Personnage median :Caballero (rappeur),Keith Urban (chanteur),Damien Chazelle (réalisateur)', desc : 'Prochaine question : Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 41 Personnage median :Rob Hopkins (enseignant ern permaculture brittanique),James Blunt (musicien brittanique),Liam Gallagher (chanteur)', desc : 'Prochaine question : Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 7 Personnage median :Daniel Radcliffe,Benedict Cumberbatch,Paul Taylor', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 25 Personnage median :Liam Gallagher (chanteur),Rob Hopkins (enseignant ern permaculture brittanique),James Blunt (musicien brittanique)', desc : 'Prochaine question : Ton personnage est-il japonais ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 154 Personnage median :Joseph Goebbels (dirigeant nazi),Dmitri Mendeleïev (chimiste russe),Stefan Zweig (écrivain autrichien)', desc : 'Prochaine question : La célébrité de ton personnage est-elle en rapport avec le monde du football ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 13 Personnage median :Manuel Neuer (gardien de foot allemand),Eden Hazard ,Bouna Sarr', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 67 Personnage median :Caballero (rappeur),Hubert Reeves,Damien Chazelle (réalisateur)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+ } 
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 466 Personnage median :Chandra Nalaàr,Ashitaka (personnaage film d\'animation),La fée clochette ', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 119 Personnage median :Jill (Va-11 Hall-a),Tracer (Overwatch),Shulk', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 85 Personnage median :Shulk,Richter Belmont (Castlevania),Tracer (Overwatch)', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 40 Personnage median :Richter Belmont (Castlevania),Shepard,Garen (jeu vidéo League of Legends)', desc : 'Prochaine question : Ton personnage est-il masculin ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 24 Personnage median :Shepard,Garen (jeu vidéo League of Legends),Richter Belmont (Castlevania)', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 3 Personnage median :Joker (Persona 5),Riven (jpersonnage de jeu vidéo),Zelda(princesse)', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 25 Personnage median :Jill (Va-11 Hall-a),Peach,Bart', desc : 'Prochaine question : Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 6 Personnage median :Adibou,Spider-man ,Zelda(princesse)', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 4 Personnage median :Le grand schtroumpf,Yoshi,Mr Burns', desc : 'Prochaine question : Ton personnage est-il masculin ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 28 Personnage median :Vel\'Koz (jeu: ligue of legend),riki,cars (personnage voiture dessin animé)', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 12 Personnage median :Alduin (personnage de Skyrim),Creeper ,riki', desc : 'Prochaine question : Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 2 Personnage median :Daniel Bryan (catcheur wwe),Monokuma (ours mécanique manga japonais Danganronpa)', desc : 'Prochaine question : Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 4 Personnage median :Un zombie ,Creeper ,Bowser (jeu vidéo Mario)', desc : 'Prochaine question : Ton personnage a-t-il des yeux ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 15 Personnage median :cars (personnage voiture dessin animé),Akinator,Vel\'Koz (jeu: ligue of legend)', desc : 'Prochaine question : Est-ce que ton personnage est un animal ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 6 Personnage median :Ganymède (oiseau compagnon de Bastion, jeu vidéo Overwatch),Vel\'Koz (jeu: ligue of legend),Simba', desc : 'Prochaine question : Ton personnage est-il masculin ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 7 Personnage median :cars (personnage voiture dessin animé),Janna (League of Legends),Tachibana(manga japonais)', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 289 Personnage median :La fée clochette ,Ant-man (super héros Marvel),Ashitaka (personnaage film d\'animation)', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 202 Personnage median :Ashitaka (personnaage film d\'animation),Ant-man (super héros Marvel),La fée clochette ', desc : 'Prochaine question : Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 79 Personnage median :Tchoupi (personnage de tchoupi),Spoke,Archie (personnage de Riverdale)', desc : 'Prochaine question : Ton personnage est-il policier ou détective ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 7 Personnage median :Dexter Morgan,Jake Peralta (personnage de Brooklyn Nine Nine),Patrick Jane (personnage de Mentalist)', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 67 Personnage median :Tchoupi (personnage de tchoupi),Archie (personnage de Riverdale),Aelita Stones ', desc : 'Prochaine question : Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 126 Personnage median :La fée clochette ,Ashitaka (personnaage film d\'animation),Ant-man (super héros Marvel)', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 51 Personnage median :Zero Kyriu (manga Vampire Knight),Ashitaka (personnaage film d\'animation),Hubert Bonnisseur de la Bath', desc : 'Prochaine question : Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 39 Personnage median :Kirua (du manga Hunter x Hunter,La fée clochette ,Cyclope (personnage des X-Men)', desc : 'Prochaine question : Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 68 Personnage median :Dobby (Personnage de Harry Potter),Rien,E.T. (alien)', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 12 Personnage median :Marvin le Martien (looney toons),Voldemort (personnage d\'Harry Potter),Supreme Leader Snoke (Personnage de star wars)', desc : 'Prochaine question : Est-ce que ton personnage est un animal ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 2 Personnage median :Grosminet (personnage de Titi),un dragon', desc : 'Prochaine question : Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 10 Personnage median :Marvin le Martien (looney toons),Voldemort (personnage d\'Harry Potter),Supreme Leader Snoke (Personnage de star wars)', desc : 'Prochaine question : Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+}, 
+ {
+text: { name: ' Personnages restants : 47 Personnage median :Po (Kong Fu Panda),Rien,E.T. (alien)', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Personnages restants : 17 Personnage median :Dingo (ami de mickey),Bob l\'éponge (personnage de dessin animé),Filémon (perso BD)', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
+]
+}, 
+ {
+text: { name: ' Personnages restants : 31 Personnage median :Rien,E.T. (alien),Po (Kong Fu Panda)', desc : 'Prochaine question : Est-ce que ton personnage est un animal ?'},HTMLclass :'light-red',collapsed : true, children : [
+]
+ } 
+]
+ } 
+]
+ } 
 ]
  } 
 ]
