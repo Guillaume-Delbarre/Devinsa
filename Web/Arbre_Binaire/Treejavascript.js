2f0c2835a4653e0a62e14abb4ac7182359bad0e9
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-23 11:09:23 +0200

    test

--- Web/Arbre_Binaire/Treejavascript.js
+++ Web/Arbre_Binaire/Treejavascript.js
@@ -8,13 +8,15 @@ text: { name: ' Personnages restants : 772 Personnage median :f,Passe-partout,Al
 
 text: { name: ' Personnages restants : 423 Personnage median :Booba ,Lorenzo,Paul Queva (étudiant INSA)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [{
 ]
-text: { name: ' Personnages restants : 352 Personnage median :Alan Walker (DJ  britannico-norvégien),Banksy (artiste brittanique),Adam Ondra', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [{}]]
+text: { name: ' Personnages restants : 352 Personnage median :Alan Walker (DJ  britannico-norvégien),Banksy (artiste brittanique),Adam Ondra', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [{
+]} 
+ ]
 text: { name: ' Personnages restants : 466 Personnage median :Andrew Wiggins (personnage livre),Alex Vaus (Personnage de serie TV:Orange is the new black),Aelita Stones ', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [{
 
 text: { name: ' Personnages restants : 119 Personnage median :Bart,Bob le Bricoleur,Arthas (Roi-Liche de Warcraft)', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [{
 ]
 text: { name: ' Personnages restants : 289 Personnage median :Alex Vaus (Personnage de serie TV:Orange is the new black),Andrew Wiggins (personnage livre),Aelita Stones ', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [{
 ]} 
- ]
- ]}
- ;
+ ]} 
+ ] } 
+ };
