b841514395854b65836d8e5d97da1be9b2cb33d9
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-23 10:56:55 +0200

    v2 js to json

--- Web/Arbre_Binaire/Treejavascript.js
+++ Web/Arbre_Binaire/Treejavascript.js
@@ -3,14 +3,20 @@ connectors: { type: 'straight' },
  node: { HTMLclass: 'nodeExample1' },
  animation: { nodeAnimation: "easeOutBounce", nodeSpeed: 700,connectorsAnimation: "bounce", connectorsSpeed: 700 }},
 nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true}, children : [
+
 text: { name: ' Personnages restants : 772 Personnage median :f,Passe-partout,Alex Keryhuel (étudiant INSA)', desc : 'Prochaine question : Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
+
 text: { name: ' Personnages restants : 423 Personnage median :Booba ,Lorenzo,Paul Queva (étudiant INSA)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+]},
 text: { name: ' Personnages restants : 352 Personnage median :Alan Walker (DJ  britannico-norvégien),Banksy (artiste brittanique),Adam Ondra', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
-} 
- ]},text: { name: ' Personnages restants : 466 Personnage median :Andrew Wiggins (personnage livre),Alex Vaus (Personnage de serie TV:Orange is the new black),Aelita Stones ', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+]} 
+ ]},
+text: { name: ' Personnages restants : 466 Personnage median :Andrew Wiggins (personnage livre),Alex Vaus (Personnage de serie TV:Orange is the new black),Aelita Stones ', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+
 text: { name: ' Personnages restants : 119 Personnage median :Bart,Bob le Bricoleur,Arthas (Roi-Liche de Warcraft)', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
-},text: { name: ' Personnages restants : 289 Personnage median :Alex Vaus (Personnage de serie TV:Orange is the new black),Andrew Wiggins (personnage livre),Aelita Stones ', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
-} 
+]},
+text: { name: ' Personnages restants : 289 Personnage median :Alex Vaus (Personnage de serie TV:Orange is the new black),Andrew Wiggins (personnage livre),Aelita Stones ', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
+]} 
  ]} 
  ] } 
  };
