14eb6d383fc7a77a4fc0830d514df969164e513a
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-25 11:16:17 +0200

    test2

--- Web/Arbre_Binaire/Treejavascript.js
+++ Web/Arbre_Binaire/Treejavascript.js
@@ -5,63 +5,27 @@ connectors: { type: 'straight' },
 nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true, children : [
 
 {
-text: { name: '772 personnage(s)', perso1 : 'Passe-partout',perso2 : \'Alex Keryhuel (étudiant INSA)\',perso2 : \'Aya Nakamura (chanteuse)\'', desc : 'Prochaine question : Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '772 personnage(s)',perso1 : 'f',perso2 : 'Passe-partout',perso3 : 'Alex Keryhuel (étudiant INSA)'', desc : 'Prochaine question : Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
 
 {
-text: { name: '423 personnage(s)',perso1 : \'Paul Queva (étudiant INSA)\',perso2 : \'Aya Nakamura (chanteuse)\',perso2 : \'Alex Keryhuel (étudiant INSA)\'', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
-
-{
-text: { name: '100 personnage(s)',perso1 : \'Caroline Aigle (pilote de chasse)\',perso2 : \'Camille Claudel\',perso2 : \'Augustin Louis Cauchy (mathématicien français)\'', desc : 'Prochaine question : Ton personnage est-il un écrivain ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '423 personnage(s)',perso1 : 'Booba ',perso2 : 'Lorenzo',perso3 : 'Paul Queva (étudiant INSA)'', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
 ]
 }, 
  {
-text: { name: '323 personnage(s)',perso1 : \'Alkpote (rappeur français)\',perso2 : \'Aya Nakamura (chanteuse)\',perso2 : \'Alex Keryhuel (étudiant INSA)\'', desc : 'Prochaine question : Ton personnage a-t-il un rapport avec le milieu du sport ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '352 personnage(s)',perso1 : 'Alan Walker (DJ  britannico-norvégien)',perso2 : 'Banksy (artiste brittanique)',perso3 : 'Adam Ondra'', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
 ]
  } 
 ]
 }, 
  {
-text: { name: '352 personnage(s)',perso1 : \'Didier Drogba (footballeur Ivoirien)\',perso2 : \'Hideo Kojima (créateur japonais de jeux vidéos)\',perso2 : \'Hubert Reeves\'', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '466 personnage(s)',perso1 : 'Andrew Wiggins (personnage livre)',perso2 : 'Alex Vaus (Personnage de serie TV:Orange is the new black)',perso3 : 'Aelita Stones '', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
 
 {
-text: { name: '121 personnage(s)',perso1 : \'Lana del Rey\',perso2 : \'Ariana Grande (chanteuse américaine)\',perso2 : \'Lil Pump (rappeur américain)\'', desc : 'Prochaine question : Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '119 personnage(s)',perso1 : 'Bart',perso2 : 'Bob le Bricoleur',perso3 : 'Arthas (Roi-Liche de Warcraft)'', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
 ]
 }, 
  {
-text: { name: '209 personnage(s)',perso1 : \'Didier Drogba (footballeur Ivoirien)\',perso2 : \'Hideo Kojima (créateur japonais de jeux vidéos)\',perso2 : \'Hubert Reeves\'', desc : 'Prochaine question : Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
-]
- } 
-]
- } 
-]
-}, 
- {
-text: { name: '466 personnage(s)',perso1 : \'Brice de Nice\',perso2 : \'Ant-man (super héros Marvel)\',perso2 : \'Malicia (X-men)\'', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
-
-{
-text: { name: '119 personnage(s)',perso1 : \'Lee Sin (League Of Legends)\',perso2 : \'Shulk\',perso2 : \'Chun-Li (jeu vidéo Steet-Fighter)\'', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
-
-{
-text: { name: '85 personnage(s)',perso1 : \'Chun-Li (jeu vidéo Steet-Fighter)\',perso2 : \'Lee Sin (League Of Legends)\',perso2 : \'Richter Belmont (Castlevania)\'', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
-]
-}, 
- {
-text: { name: '28 personnage(s)',perso1 : \'Ganymède (oiseau compagnon de Bastion, jeu vidéo Overwatch)\',perso2 : \'Vel\'Koz (jeu: ligue of legend)\',perso2 : \'cars (personnage voiture dessin animé)\'', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
-]
- } 
-]
-}, 
- {
-text: { name: '289 personnage(s)',perso1 : \'Brice de Nice\',perso2 : \'Ant-man (super héros Marvel)\',perso2 : \'Malicia (X-men)\'', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
-
-{
-text: { name: '202 personnage(s)',perso1 : \'Brice de Nice\',perso2 : \'Malicia (X-men)\',perso2 : \'Ant-man (super héros Marvel)\'', desc : 'Prochaine question : Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
-]
-}, 
- {
-text: { name: '68 personnage(s)',perso1 : \'Le soleil des Télétubbies\',perso2 : \'Plutot\',perso2 : \'E.T. (alien)\'', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
-]
- } 
+text: { name: '289 personnage(s)',perso1 : 'Alex Vaus (Personnage de serie TV:Orange is the new black)',perso2 : 'Andrew Wiggins (personnage livre)',perso3 : 'Aelita Stones '', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
 ]
  } 
 ]
