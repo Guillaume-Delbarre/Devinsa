77f8237e40ca77603025bb00f486ad11731e7cea
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-25 11:13:10 +0200

    final

--- Web/Arbre_Binaire/Treejavascript.js
+++ Web/Arbre_Binaire/Treejavascript.js
@@ -5,30 +5,30 @@ connectors: { type: 'straight' },
 nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 772 Personnage median :Passe-partout,Alex Keryhuel (étudiant INSA),Aya Nakamura (chanteuse)', desc : 'Prochaine question : Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '772 personnage(s)',perso1 : \'Passe-partout\',perso2 : \'Alex Keryhuel (étudiant INSA)\',perso2 : \'Aya Nakamura (chanteuse)\'', desc : 'Prochaine question : Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 423 Personnage median :Paul Queva (étudiant INSA),Aya Nakamura (chanteuse),Alex Keryhuel (étudiant INSA)', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '423 personnage(s)',perso1 : \'Paul Queva (étudiant INSA)\',perso2 : \'Aya Nakamura (chanteuse)\',perso2 : \'Alex Keryhuel (étudiant INSA)\'', desc : 'Prochaine question : Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 100 Personnage median :Caroline Aigle (pilote de chasse),Camille Claudel,Augustin Louis Cauchy (mathématicien français)', desc : 'Prochaine question : Ton personnage est-il un écrivain ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '100 personnage(s)',perso1 : \'Caroline Aigle (pilote de chasse)\',perso2 : \'Camille Claudel\',perso2 : \'Augustin Louis Cauchy (mathématicien français)\'', desc : 'Prochaine question : Ton personnage est-il un écrivain ?'},HTMLclass :'light-green',collapsed : true, children : [
 ]
 }, 
  {
-text: { name: ' Personnages restants : 323 Personnage median :Alkpote (rappeur français),Aya Nakamura (chanteuse),Alex Keryhuel (étudiant INSA)', desc : 'Prochaine question : Ton personnage a-t-il un rapport avec le milieu du sport ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '323 personnage(s)',perso1 : \'Alkpote (rappeur français)\',perso2 : \'Aya Nakamura (chanteuse)\',perso2 : \'Alex Keryhuel (étudiant INSA)\'', desc : 'Prochaine question : Ton personnage a-t-il un rapport avec le milieu du sport ?'},HTMLclass :'light-red',collapsed : true, children : [
 ]
  } 
 ]
 }, 
  {
-text: { name: ' Personnages restants : 352 Personnage median :Didier Drogba (footballeur Ivoirien),Hideo Kojima (créateur japonais de jeux vidéos),Hubert Reeves', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '352 personnage(s)',perso1 : \'Didier Drogba (footballeur Ivoirien)\',perso2 : \'Hideo Kojima (créateur japonais de jeux vidéos)\',perso2 : \'Hubert Reeves\'', desc : 'Prochaine question : Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 121 Personnage median :Lana del Rey,Ariana Grande (chanteuse américaine),Lil Pump (rappeur américain)', desc : 'Prochaine question : Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '121 personnage(s)',perso1 : \'Lana del Rey\',perso2 : \'Ariana Grande (chanteuse américaine)\',perso2 : \'Lil Pump (rappeur américain)\'', desc : 'Prochaine question : Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
 ]
 }, 
  {
-text: { name: ' Personnages restants : 209 Personnage median :Didier Drogba (footballeur Ivoirien),Hideo Kojima (créateur japonais de jeux vidéos),Hubert Reeves', desc : 'Prochaine question : Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '209 personnage(s)',perso1 : \'Didier Drogba (footballeur Ivoirien)\',perso2 : \'Hideo Kojima (créateur japonais de jeux vidéos)\',perso2 : \'Hubert Reeves\'', desc : 'Prochaine question : Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
 ]
  } 
 ]
@@ -36,30 +36,30 @@ text: { name: ' Personnages restants : 209 Personnage median :Didier Drogba (foo
 ]
 }, 
  {
-text: { name: ' Personnages restants : 466 Personnage median :Brice de Nice,Ant-man (super héros Marvel),Malicia (X-men)', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '466 personnage(s)',perso1 : \'Brice de Nice\',perso2 : \'Ant-man (super héros Marvel)\',perso2 : \'Malicia (X-men)\'', desc : 'Prochaine question : Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 119 Personnage median :Lee Sin (League Of Legends),Shulk,Chun-Li (jeu vidéo Steet-Fighter)', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '119 personnage(s)',perso1 : \'Lee Sin (League Of Legends)\',perso2 : \'Shulk\',perso2 : \'Chun-Li (jeu vidéo Steet-Fighter)\'', desc : 'Prochaine question : Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 85 Personnage median :Chun-Li (jeu vidéo Steet-Fighter),Lee Sin (League Of Legends),Richter Belmont (Castlevania)', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '85 personnage(s)',perso1 : \'Chun-Li (jeu vidéo Steet-Fighter)\',perso2 : \'Lee Sin (League Of Legends)\',perso2 : \'Richter Belmont (Castlevania)\'', desc : 'Prochaine question : Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
 ]
 }, 
  {
-text: { name: ' Personnages restants : 28 Personnage median :Ganymède (oiseau compagnon de Bastion, jeu vidéo Overwatch),Vel\'Koz (jeu: ligue of legend),cars (personnage voiture dessin animé)', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '28 personnage(s)',perso1 : \'Ganymède (oiseau compagnon de Bastion, jeu vidéo Overwatch)\',perso2 : \'Vel\'Koz (jeu: ligue of legend)\',perso2 : \'cars (personnage voiture dessin animé)\'', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
 ]
  } 
 ]
 }, 
  {
-text: { name: ' Personnages restants : 289 Personnage median :Brice de Nice,Ant-man (super héros Marvel),Malicia (X-men)', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '289 personnage(s)',perso1 : \'Brice de Nice\',perso2 : \'Ant-man (super héros Marvel)\',perso2 : \'Malicia (X-men)\'', desc : 'Prochaine question : Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
 
 {
-text: { name: ' Personnages restants : 202 Personnage median :Brice de Nice,Malicia (X-men),Ant-man (super héros Marvel)', desc : 'Prochaine question : Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '202 personnage(s)',perso1 : \'Brice de Nice\',perso2 : \'Malicia (X-men)\',perso2 : \'Ant-man (super héros Marvel)\'', desc : 'Prochaine question : Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
 ]
 }, 
  {
-text: { name: ' Personnages restants : 68 Personnage median :Le soleil des Télétubbies,Plutot,E.T. (alien)', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '68 personnage(s)',perso1 : \'Le soleil des Télétubbies\',perso2 : \'Plutot\',perso2 : \'E.T. (alien)\'', desc : 'Prochaine question : Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
 ]
  } 
 ]
