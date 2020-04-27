0757668b2c3e9bf3553ffb9983d161d55322c761
zoepj <zoe.pesleux@insa-rennes.fr>
2020-04-27 12:09:54 +0100

    Différence

--- ScriptPython/differences.py
+++ ScriptPython/differences.py
@@ -6,9 +6,12 @@ import os
 import sys
 from sklearn.metrics import pairwise_distances
 
-def differencesSelectionCluster(L1,L2):
+df = pd.read_csv("../Donnees/kmeans.csv", sep = ";", header=0, index_col=0, encoding = 'utf-8')
 
-def differencesSelectionCluster(L1,L2):
+def differences2Cluster(L1,L2):
+    
+
+def differences2Selection(L1,L2):
 
 def differencesSelectionCluster(L1,L2):
     
@@ -19,6 +22,16 @@ if __name__ == '__main__':
     if (len(sys.argv) == 3):
         L1=int(sys.argv[1])
         L2=int(sys.argv[2])
-        differences(L1,L2)
+        if(isinstance(L1[0], int) and isinstance(L2[0], int)):
+            differences2Cluster(L1,L2)
+        elif(isinstance(L1[0], str) and isinstance(L2[0], str)):
+            differences2Selection(L1,L2)
+        elif(isinstance(L1[0], str) and isinstance(L2[0], int)):
+            differencesSelectionCluster(L1,L2)
+        elif(isinstance(L1[0], int) and isinstance(L2[0], str)):
+            differencesSelectionCluster(L2,L1)
+        else:
+            raise ValueError("Erreur dans les type des paramètres")
+
     else:
-        differences(["Batman"],[1])
+        differences2Cluster([2],[1])
