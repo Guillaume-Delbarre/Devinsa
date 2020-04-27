d0f4001a709ba6c9c57e34f7062c0343b5da747a
zoepj <zoe.pesleux@insa-rennes.fr>
2020-04-27 12:08:01 +0100

    difference

---
+++ ScriptPython/differences.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import os
+import sys
+from sklearn.metrics import pairwise_distances
+
+def differencesSelectionCluster(L1,L2):
+
+def differencesSelectionCluster(L1,L2):
+
+def differencesSelectionCluster(L1,L2):
+    
+
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 3):
+        L1=int(sys.argv[1])
+        L2=int(sys.argv[2])
+        differences(L1,L2)
+    else:
+        differences(["Batman"],[1])
