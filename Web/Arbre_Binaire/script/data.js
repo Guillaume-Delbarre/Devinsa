f2857e4add9a3fb5e3f73b7feb24e732818a2995
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-29 16:32:31 +0200

    arbre

--- Web/Arbre_Binaire/script/data.js
+++ Web/Arbre_Binaire/script/data.js
@@ -5,11 +5,11 @@ connectors: { type: 'step' },
 nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true, children : [
 
 {
-text: { name: '113 personnage(s)',perso1 : 'Jill (Va-11 Hall-a)',perso2 : 'Tracer (Overwatch)',perso3 : 'Altair', desc : 'Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '114 personnage(s)',perso1 : 'Tracer (Overwatch)',perso2 : 'Altair',perso3 : 'Jill (Va-11 Hall-a)', desc : 'Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text : {name : 'Fin'}, collapsed: true}]
 }, 
  {
-text: { name: '370 personnage(s)',perso1 : 'Tryphon Tournesol',perso2 : 'Tyler Durden (perso du film fight club)[',perso3 : 'Ford Prefect (perso du Guide du voyageur galactique)', desc : 'Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: '371 personnage(s)',perso1 : 'Tryphon Tournesol',perso2 : 'Tyler Durden (perso du film fight club)[',perso3 : 'Ford Prefect (perso du Guide du voyageur galactique)', desc : 'Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text : {name : 'Fin'}, collapsed: true}]
  } 
 ] } 
