69a35aca6782a351cfc71361de88434b8c9a3982
Ougo <hugo.martineau@insa-rennes.fr>
2020-04-29 16:05:10 +0200

    arbre

--- Web/Arbre_Binaire/script/data.js
+++ Web/Arbre_Binaire/script/data.js
@@ -5,12 +5,210 @@ connectors: { type: 'step' },
 nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true, children : [
 
 {
-text: { name: '113 personnage(s)',perso0 : 'Jill (Va-11 Hall-a)', desc : 'Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
+text: { name: '113 personnage(s)',perso1 : 'Jill (Va-11 Hall-a)',perso2 : 'Tracer (Overwatch)',perso3 : 'Altair', desc : 'Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '4 personnage(s)',perso1 : 'Arthur Morgan (personnage de Red dead Redemption 2)',perso2 : 'Batman',perso3 : 'Eric Théodore Cartman ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '1 personnage(s)',perso1 : 'Monsieur Oui', desc : 'Ton personnage est-il un écrivain ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '1 personnage(s)',perso1 : 'Monsieur Oui', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '1 personnage(s)',perso1 : 'Monsieur Oui', desc : 'Ton personnage est-il policier ou détective ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text : {name : 'Fin'}, collapsed: true}]
 }, 
  {
-text: { name: '370 personnage(s)',perso0 : 'Tryphon Tournesol', desc : 'Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+text: { name: ' Aucun personnage '}, collapsed : true
+
+ } 
+]
+}, 
+ {
+text: { name: ' Aucun personnage '}, collapsed : true
+
+ } 
+]
+}, 
+ {
+text: { name: '2 personnage(s)',perso1 : 'Batman',perso2 : 'Eric Théodore Cartman ', desc : 'Ton personnage a-t-il un rapport avec le milieu du sport ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Aucun personnage '}, collapsed : true
+
+}, 
+ {
+text: { name: ' Aucun personnage '}, collapsed : true
+
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: '9 personnage(s)',perso1 : 'Rayman',perso2 : 'Ryu',perso3 : 'Vlad III (roi de transylvanie)', desc : 'Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Aucun personnage '}, collapsed : true
+
+}, 
+ {
+text: { name: '4 personnage(s)',perso1 : 'Julien Lepers',perso2 : 'Pikachu (personnage de manga)',perso3 : 'Sonic ', desc : 'Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: '1 personnage(s)',perso1 : 'Julien Lepers', desc : 'Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: ' Aucun personnage '}, collapsed : true
+
+}, 
+ {
+text: { name: ' Aucun personnage '}, collapsed : true
+
+ } 
+]
+}, 
+ {
+text: { name: '1 personnage(s)',perso1 : 'Mario Bros (perso jeux vidéo)', desc : 'La célébrité de ton personnage est-elle en rapport avec le monde du football ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Aucun personnage '}, collapsed : true
+
+}, 
+ {
+text: { name: '1 personnage(s)',perso1 : 'Mario Bros (perso jeux vidéo)', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text : {name : 'Fin'}, collapsed: true}]
+ } 
+]
+ } 
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: '370 personnage(s)',perso1 : 'Tryphon Tournesol',perso2 : 'Tyler Durden (perso du film fight club)[',perso3 : 'Ford Prefect (perso du Guide du voyageur galactique)', desc : 'Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: '224 personnage(s)',perso1 : 'Ford Prefect (perso du Guide du voyageur galactique)',perso2 : 'Abraracourcix',perso3 : 'Tyler Durden (perso du film fight club)[', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '17 personnage(s)',perso1 : 'Kylo Ren (perso de star wars)',perso2 : 'Lucifer ',perso3 : 'The Punisher', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '7 personnage(s)',perso1 : 'Dexter Morgan',perso2 : 'Dracula',perso3 : 'Moriarty (ennemi de sherlock Holmes)', desc : 'Ton personnage est-il masculin ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '7 personnage(s)',perso1 : 'Dexter Morgan',perso2 : 'Dracula',perso3 : 'Moriarty (ennemi de sherlock Holmes)', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-green',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+}, 
+ {
+text: { name: ' Aucun personnage '}, collapsed : true
+
+ } 
+]
+}, 
+ {
+text: { name: ' Aucun personnage '}, collapsed : true
+
+ } 
+]
+}, 
+ {
+text: { name: '136 personnage(s)',perso1 : 'Abraracourcix',perso2 : 'Tuco (western: le bon, la brute et le truand)',perso3 : 'Ant-man (super héros Marvel)', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: ' Aucun personnage '}, collapsed : true
+
+}, 
+ {
+text: { name: '50 personnage(s)',perso1 : 'Green Arrow ',perso2 : 'Eyeshield 21',perso3 : 'Capitaine Haddock', desc : 'Est-ce que ton personnage est un animal ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: '19 personnage(s)',perso1 : 'Thor ',perso2 : 'Gandalf (le seigneur des anneaux)',perso3 : 'Jaime Lannister ', desc : 'Ton personnage est-il masculin ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+}, 
+ {
+text: { name: '26 personnage(s)',perso1 : 'Eyeshield 21',perso2 : 'Capitaine Haddock',perso3 : 'Gandalf', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+ } 
+]
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: '63 personnage(s)',perso1 : 'L\'Œil glauque (Mascotte de festival)',perso2 : 'E.T. (alien)',perso3 : 'Olaf (la reine des neiges)', desc : 'Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: '11 personnage(s)',perso1 : 'Supreme Leader Snoke (Personnage de star wars)',perso2 : 'Voldemort (personnage d\'Harry Potter)',perso3 : 'Marvin le Martien (looney toons)', desc : 'Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '4 personnage(s)',perso1 : 'Sauron (le seigneur des anneaux)',perso2 : 'Voldemort (personnage d\'Harry Potter)',perso3 : 'Walter White (Personnage de Breaking Bad)', desc : 'Ton personnage est-il policier ou détective ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '2 personnage(s)',perso1 : 'Dark Vador',perso2 : 'Walter White (Personnage de Breaking Bad)', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+}, 
+ {
+text: { name: '1 personnage(s)',perso1 : 'Sauron (le seigneur des anneaux)', desc : 'Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+ } 
+]
+}, 
+ {
+text: { name: '2 personnage(s)',perso1 : 'Gollum',perso2 : 'Grosminet (personnage de Titi)', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: '2 personnage(s)',perso1 : 'Gollum',perso2 : 'Grosminet (personnage de Titi)', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+}, 
+ {
+text: { name: ' Aucun personnage '}, collapsed : true
+
+ } 
+]
+ } 
+]
+}, 
+ {
+text: { name: '41 personnage(s)',perso1 : 'Olaf (la reine des neiges)',perso2 : 'E.T. (alien)',perso3 : 'Le soleil des Télétubbies', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: '13 personnage(s)',perso1 : 'Dingo (ami de mickey)',perso2 : 'Doraemon',perso3 : 'Filémon (perso BD)', desc : 'Est-ce que ton personnage est un animal ?'},HTMLclass :'light-green',collapsed : true, children : [
+
+{
+text: { name: '4 personnage(s)',perso1 : 'Dingo (ami de mickey)',perso2 : 'Jean-Luc Picard (Star Trek)',perso3 : 'popeye', desc : 'Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+}, 
+ {
+text: { name: '6 personnage(s)',perso1 : 'Bob l\'éponge (personnage de dessin animé)',perso2 : 'Franklin',perso3 : 'Schtroumpf', desc : 'Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+ } 
+]
+}, 
+ {
+text: { name: '26 personnage(s)',perso1 : 'Le soleil des Télétubbies',perso2 : 'E.T. (alien)',perso3 : 'Olaf (la reine des neiges)', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-red',collapsed : true, children : [
+
+{
+text: { name: '9 personnage(s)',perso1 : 'Idéfix',perso2 : 'Plutot',perso3 : 'Po (Kong Fu Panda)', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+}, 
+ {
+text: { name: '14 personnage(s)',perso1 : 'Le soleil des Télétubbies',perso2 : 'Marvin (H2G2)',perso3 : 'Monsieur Non', desc : 'Est-ce que ton personnage est un animal ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text : {name : 'Fin'}, collapsed: true}]
+ } 
+]
+ } 
+]
+ } 
+]
+ } 
+]
  } 
 ] } 
  };
