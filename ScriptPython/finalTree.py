4753859f63fec9fe6cecc518fa9097fdc5afa89d
Ougo <hugo.martineau@insa-rennes.fr>
2020-05-10 10:57:55 +0200

    script inutile

--- ScriptPython/finalTree.py
+++ ScriptPython/finalTree.py
@@ -158,7 +158,7 @@ def exemples(perso):
     remove = np.full(perso.shape[1],0)
     print(remove.shape)
     print(perso.shape)
-    perso = np.delete(perso,np.where(perso == remove))
+    perso = np.delete(perso,np.where(perso == remove),axis = 0)
     print(perso.shape)
     moyen = np.mean(perso,0)
     dist = distScalaire(perso,moyen)
