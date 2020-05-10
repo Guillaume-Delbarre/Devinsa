9f83d7b9cc6f1cddf7b7a2ea673d0aeb00b1632b
hugo3m <hugo.martineau@insa-rennes.fr>
2020-05-10 11:32:52 +0200

    nouvelle distance

--- Web/Arbre_Binaire/script/data.js
+++ Web/Arbre_Binaire/script/data.js
@@ -3,9 +3,9 @@ connectors: { type: 'step' },
  node: { HTMLclass: 'nodeExample1' },
  animation: { nodeAnimation: "easeOutBounce", nodeSpeed: 700,connectorsAnimation: "bounce", connectorsSpeed: 700 }},
 nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : true, children : [
-{text: { name: '1078 personnage(s)',perso1 : 'Abigail Marston ',perso2 : ' Samir Loussif ',perso3 : 'A rien lol', desc : 'Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
-{text: { name: '737 personnage(s)',perso1 : 'Abigail Marston ',perso2 : ' Samir Loussif ',perso3 : 'A rien lol', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
-{text: { name: '411 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il un écrivain ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text: { name: '1078 personnage(s)',perso1 : 'Michel Blanc (acteur français)',perso2 : 'Roselyne Bachelot',perso3 : 'Brigitte Macron', desc : 'Ton personnage est-il français ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text: { name: '737 personnage(s)',perso1 : 'Phillipe Poutou',perso2 : 'Michel Blanc (acteur français)',perso3 : 'Brigitte Macron', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text: { name: '411 personnage(s)',perso1 : 'Jean-Claude François ',perso2 : 'Marie-France Pisier ',perso3 : 'Cyril Dumoulin ', desc : 'Ton personnage est-il un écrivain ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '348 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '320 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il policier ou détective ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
@@ -13,7 +13,7 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '391 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage fait-il de la politique ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '391 personnage(s)',perso1 : 'Marie-France Pisier ',perso2 : 'Jean-Claude François ',perso3 : 'Cyril Dumoulin ', desc : 'Ton personnage fait-il de la politique ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '350 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il devenu connu grâce à Internet ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
  {text: { name: '365 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il devenu connu grâce à Internet ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
@@ -22,18 +22,18 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '636 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il un rapport avec le milieu du sport ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '399 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il un footballeur ?'},HTMLclass :'light-green',collapsed : true, children : [
+ {text: { name: '636 personnage(s)',perso1 : 'Michel Blanc (acteur français)',perso2 : 'Luc Besson',perso3 : 'Phillipe Poutou', desc : 'Ton personnage a-t-il un rapport avec le milieu du sport ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text: { name: '399 personnage(s)',perso1 : 'Le petit Grégory ',perso2 : 'Cyril Dumoulin ',perso3 : 'Cartman ', desc : 'Ton personnage est-il un footballeur ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '343 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il plus de 35 ans ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
  {text: { name: '377 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il plus de 50 ans ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
 }, 
- {text: { name: '551 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il devenu connu grâce à Internet ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '551 personnage(s)',perso1 : 'Yann Barthes (presentateur télé)',perso2 : 'Luc Besson',perso3 : 'Phillipe Poutou', desc : 'Ton personnage est-il devenu connu grâce à Internet ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '400 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il un garçon ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
- {text: { name: '499 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il célèbre  ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '499 personnage(s)',perso1 : 'Phillipe Poutou',perso2 : 'Luc Besson',perso3 : 'Yann Barthes (presentateur télé)', desc : 'Ton personnage est-il célèbre  ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
  } 
@@ -41,8 +41,8 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '665 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '458 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
+ {text: { name: '665 personnage(s)',perso1 : 'Hergé (dessinateur Tintin)',perso2 : 'Lana del Rey',perso3 : 'Ramanujan', desc : 'Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text: { name: '458 personnage(s)',perso1 : 'Marie-France Pisier ',perso2 : 'Jean-Claude François ',perso3 : 'Cyril Dumoulin ', desc : 'Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '383 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il plus de 35 ans ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '374 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
@@ -50,16 +50,16 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '406 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il musicien ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '406 personnage(s)',perso1 : 'Cartman ',perso2 : 'Le petit Grégory ',perso3 : 'Cyril Dumoulin ', desc : 'Ton personnage est-il musicien ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '372 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
- {text: { name: '387 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '387 personnage(s)',perso1 : 'Cyril Dumoulin ',perso2 : 'Le petit Grégory ',perso3 : 'Cartman ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
  } 
 ]
 }, 
- {text: { name: '549 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '549 personnage(s)',perso1 : 'Hergé (dessinateur Tintin)',perso2 : 'Ramanujan',perso3 : 'Jean Paul 2', desc : 'Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '391 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '340 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
@@ -67,10 +67,10 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '500 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'La célébrité de ton personnage est-elle en rapport avec le monde du football ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '500 personnage(s)',perso1 : 'Le petit Grégory ',perso2 : 'Cyril Dumoulin ',perso3 : 'Cartman ', desc : 'La célébrité de ton personnage est-elle en rapport avec le monde du football ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '432 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
- {text: { name: '487 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '487 personnage(s)',perso1 : 'Cyril Dumoulin ',perso2 : 'Cartman ',perso3 : 'Le petit Grégory ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
  } 
@@ -80,8 +80,8 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '771 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '490 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
+ {text: { name: '771 personnage(s)',perso1 : 'Jill (Va-11 Hall-a)',perso2 : 'Tracer (Overwatch)',perso3 : 'Zelda', desc : 'Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text: { name: '490 personnage(s)',perso1 : 'Aquarius ',perso2 : 'Général Shepard (Mass Effect)',perso3 : 'Libellule ', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '431 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '396 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il masculin ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '389 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
@@ -118,8 +118,8 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '662 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '567 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
+ {text: { name: '662 personnage(s)',perso1 : 'Tryphon Tournesol',perso2 : 'Zack Branigan',perso3 : 'Ford Prefect (perso du Guide du voyageur galactique)', desc : 'Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text: { name: '567 personnage(s)',perso1 : 'Zack Branigan',perso2 : 'Général Shepard (Mass Effect)',perso3 : 'Aquarius ', desc : 'Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '420 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il policier ou détective ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '334 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
@@ -127,7 +127,7 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '468 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '468 personnage(s)',perso1 : 'Libellule ',perso2 : 'Aquarius ',perso3 : 'Général Shepard (Mass Effect)', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '421 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
  {text: { name: '409 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
@@ -136,7 +136,7 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '430 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '430 personnage(s)',perso1 : 'Aquarius ',perso2 : 'Libellule ',perso3 : 'Général Shepard (Mass Effect)', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '349 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Est-ce que ton personnage est un animal ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '330 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Abigail Marston ', desc : 'Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
