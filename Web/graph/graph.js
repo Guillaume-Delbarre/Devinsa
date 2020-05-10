2f17cb7f5ba5e1d0e4b5fe8097a3a88582a9af1f
nathan <nathan.lemaire@insa-rennes.fr>
2020-05-10 02:07:56 +0200

    a

--- Web/graph/graph.js
+++ Web/graph/graph.js
@@ -228,7 +228,7 @@ $(document).ready( function () {
 	$('#listQusetion').on('click', 'tr', function () {
 		$(this).toggleClass('selected');
     });
-	ta.on('select', function ( e, dt, type, indexes ) {
+	ta.on('select.dt', function ( e, dt, type, indexes ) {
 		alert("ici");
 		if (type === 'row') {
 			var a = ta.rows(indexes).data();
