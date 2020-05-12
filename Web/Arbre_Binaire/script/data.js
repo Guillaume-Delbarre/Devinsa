aade8fbababa4aa6bab261c9e4153bb675125796
hugo3m <hugo.martineau@insa-rennes.fr>
2020-05-12 09:30:23 +0200

    arbre

--- Web/Arbre_Binaire/script/data.js
+++ Web/Arbre_Binaire/script/data.js
@@ -81,7 +81,7 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '665 personnage(s)',perso1 : 'Bob Dylan (chanteur)',perso2 : 'Andy Serkis (acteur brittanique)',perso3 : 'Black Star ', desc : 'Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '664 personnage(s)',perso1 : 'Caca bouda ',perso2 : 'Bill Gates (Fondateur de Microsoft)',perso3 : 'Black Star ', desc : 'Ton personnage est-il américain ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '458 personnage(s)',perso1 : 'Amiral Yamamoto ',perso2 : 'Andy Serkis (acteur brittanique)',perso3 : 'Axoloto ', desc : 'Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '383 personnage(s)',perso1 : 'Angelina Jolie (actrice)',perso2 : 'Aleister Crowley ',perso3 : 'Amaterasu Okami ', desc : 'Ton personnage a-t-il plus de 35 ans ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '374 personnage(s)',perso1 : 'Andy ',perso2 : 'Amaterasu Okami ',perso3 : 'Aleister Crowley ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
@@ -119,7 +119,7 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '549 personnage(s)',perso1 : 'Atomic Betty ',perso2 : 'Attila (roi de huns)',perso3 : 'Benyamin Netanyahou (premier ministre Israël))', desc : 'Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '548 personnage(s)',perso1 : 'Aspicot (pokemon)  ',perso2 : 'Atomic Betty ',perso3 : 'Benedict Cumberbatch', desc : 'Ton personnage est-il anglais ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '391 personnage(s)',perso1 : 'Amiral Yamamoto ',perso2 : 'Aissa Mandi	 ',perso3 : 'Aleister Crowley ', desc : 'Ton personnage est-il acteur/actrice ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '340 personnage(s)',perso1 : 'Aja Naomi King (actrice américaine)',perso2 : 'Ada Lovelace (comtesse brittanique programmeuse)',perso3 : 'Aissa Mandi	 ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '322 personnage(s)',perso1 : 'A rien lol',perso2 : 'Abigail Marston ',perso3 : 'Ada Lovelace (comtesse brittanique programmeuse)', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
@@ -137,16 +137,16 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '500 personnage(s)',perso1 : 'Annie Cordy',perso2 : 'Ant-man ',perso3 : 'Aspicot (pokemon)  ', desc : 'La célébrité de ton personnage est-elle en rapport avec le monde du football ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '432 personnage(s)',perso1 : 'Aissa Mandi	 ',perso2 : 'Andrew Martin ',perso3 : 'Angelina Jolie (actrice)', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
-{text: { name: '368 personnage(s)',perso1 : 'Aleister Crowley ',perso2 : 'Amadou Toumani Touré ',perso3 : 'A rien lol', desc : 'Ton personnage est-il allemand ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '499 personnage(s)',perso1 : 'Annie Cordy',perso2 : 'Ashura Ôtsutsuki ',perso3 : 'Anne Hidalgo ', desc : 'La célébrité de ton personnage est-elle en rapport avec le monde du football ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text: { name: '431 personnage(s)',perso1 : 'André Mathevet ',perso2 : 'Andy ',perso3 : 'Ahmed mansour ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text: { name: '367 personnage(s)',perso1 : 'Al-Khwarizmi (mathématicien arabe du 10e siècle)',perso2 : 'Aleister Crowley ',perso3 : ' Samir Loussif ', desc : 'Ton personnage est-il allemand ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
  {text: { name: '373 personnage(s)',perso1 : 'Alan Walker (DJ  britannico-norvégien)',perso2 : 'Abel ',perso3 : 'Ahmed Chawki ', desc : 'Ton personnage a-t-il plus de 35 ans ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
 }, 
- {text: { name: '487 personnage(s)',perso1 : 'Andy ',perso2 : 'Angela Merkel',perso3 : 'Amaterasu (déesse japonaise du soleil)', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '392 personnage(s)',perso1 : 'Amadou Toumani Touré ',perso2 : 'Amaterasu Okami ',perso3 : 'Aleister Crowley ', desc : 'Ton personnage est-il allemand ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '486 personnage(s)',perso1 : 'Andrew Martin ',perso2 : 'Andy ',perso3 : 'Amadou Toumani Touré ', desc : 'Ton personnage est-il décédé ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text: { name: '391 personnage(s)',perso1 : 'Amaterasu (déesse japonaise du soleil)',perso2 : 'Amiral Yamamoto ',perso3 : 'Alcibiade (homme politique et général athénien)', desc : 'Ton personnage est-il allemand ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
  {text: { name: '405 personnage(s)',perso1 : 'Ahmed mansour ',perso2 : 'Aissa Mandi	 ',perso3 : 'Aja Naomi King (actrice américaine)', desc : 'Ton personnage a-t-il plus de 35 ans ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
@@ -163,11 +163,11 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  {text: { name: '771 personnage(s)',perso1 : 'Agent 47',perso2 : 'Bob l\'éponge (personnage de dessin animé)',perso3 : 'Anne Hidalgo ', desc : 'Ton personnage est-il un personnage de jeu vidéo ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '490 personnage(s)',perso1 : 'Amy Reid ',perso2 : ' Samir Loussif ',perso3 : 'Aissa Mandi	 ', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '431 personnage(s)',perso1 : 'Ahmed Chawki ',perso2 : 'Actarus',perso3 : 'Adibou', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
-{text: { name: '396 personnage(s)',perso1 : 'Aleister Crowley ',perso2 : 'Abel ',perso3 : 'Actarus', desc : 'Ton personnage est-il masculin ?'},HTMLclass :'light-green',collapsed : true, children : [
-{text: { name: '389 personnage(s)',perso1 : 'Aleister Crowley ',perso2 : 'Abel ',perso3 : 'Actarus', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-green',collapsed : true, children : [
+{text: { name: '395 personnage(s)',perso1 : 'Aleister Crowley ',perso2 : 'Abel ',perso3 : 'Actarus', desc : 'Ton personnage est-il masculin ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text: { name: '388 personnage(s)',perso1 : 'Aleister Crowley ',perso2 : 'Abel ',perso3 : 'Actarus', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '331 personnage(s)',perso1 : ' Samir Loussif ', desc : 'Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
- {text: { name: '377 personnage(s)',perso1 : 'Albus Dumbledore (personnage de Harry Potter)',perso2 : 'Abel ',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '376 personnage(s)',perso1 : 'Albus Dumbledore (personnage de Harry Potter)',perso2 : 'Abel ',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
 }, 
@@ -238,37 +238,37 @@ nodeStructure : {text: { name: 'Ton personnage est-il réel ?' }, collapsed : tr
  } 
 ]
 }, 
- {text: { name: '662 personnage(s)',perso1 : 'Aelita Stones ',perso2 : 'Black Widow (perso de l\'univers Marvel)',perso3 : 'Albator', desc : 'Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '567 personnage(s)',perso1 : 'Ayano Kannagi ',perso2 : 'Akinator',perso3 : 'Ahmed Chawki ', desc : 'Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
-{text: { name: '420 personnage(s)',perso1 : 'Amy Reid ',perso2 : 'Alex (Totally Spies)',perso3 : 'Abigail Marston ', desc : 'Ton personnage est-il policier ou détective ?'},HTMLclass :'light-green',collapsed : true, children : [
+ {text: { name: '661 personnage(s)',perso1 : 'Aelita Stones ',perso2 : 'Black Star ',perso3 : 'Anne Hidalgo ', desc : 'Ton personnage a-t-il des cheveux ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text: { name: '565 personnage(s)',perso1 : 'Akinator',perso2 : 'Ahmed Chawki ',perso3 : 'Atomic Betty ', desc : 'Ton personnage est-il un personnage de série télévisée ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text: { name: '417 personnage(s)',perso1 : 'Abigail Marston ',perso2 : 'Amaterasu Okami ',perso3 : 'Amiral Yamamoto ', desc : 'Ton personnage est-il policier ou détective ?'},HTMLclass :'light-green',collapsed : true, children : [
 {text: { name: '334 personnage(s)',perso1 : 'Abigail Marston ',perso2 : 'Actarus',perso3 : 'Ahmed Chawki ', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-green',collapsed : true, children : [
-{text: { name: '326 personnage(s)',perso1 : 'Abel ',perso2 : 'Abigail Marston ',perso3 : 'Actarus', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+{text: { name: '325 personnage(s)',perso1 : 'Abel ',perso2 : 'Abigail Marston ',perso3 : 'Actarus', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
  {text: { name: '324 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'Abel ',perso3 : 'Actarus', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
 }, 
- {text: { name: '407 personnage(s)',perso1 : 'Aleister Crowley ',perso2 : 'Abel ',perso3 : 'Amaterasu Okami ', desc : 'Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '396 personnage(s)',perso1 : 'Abel ',perso2 : 'Ahmed Chawki ',perso3 : 'Aissa Mandi	 ', desc : 'Ton personnage fait-il des combats ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '404 personnage(s)',perso1 : 'Abel ',perso2 : 'Alex Vaus (Personnage de serie TV:Orange is the new black)',perso3 : 'Amadou Toumani Touré ', desc : 'Ton personnage a-t-il la peau blanche ?'},HTMLclass :'light-red',collapsed : true, children : [
+{text: { name: '394 personnage(s)',perso1 : 'A rien lol',perso2 : 'Abel ',perso3 : 'Alex Vaus (Personnage de serie TV:Orange is the new black)', desc : 'Ton personnage fait-il des combats ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
- {text: { name: '360 personnage(s)',perso1 : 'Abigail Marston ',perso2 : 'Aelita Stones ',perso3 : 'Ahmed mansour ', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '358 personnage(s)',perso1 : 'Abel ',perso2 : 'Abigail Marston ',perso3 : 'Aelita Stones ', desc : 'Ton personnage porte-t-il des chaussures ?'},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
  } 
 ]
 }, 
  {text: { name: '468 personnage(s)',perso1 : 'Anne Hidalgo ',perso2 : 'Aladdin',perso3 : 'Aleister Crowley ', desc : 'Ton personnage possède-t-il des armes ?'},HTMLclass :'light-red',collapsed : true, children : [
-{text: { name: '421 personnage(s)',perso1 : 'Abigail Marston ',perso2 : 'Andy ',perso3 : 'Ahmed mansour ', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [
-{text: { name: '385 personnage(s)',perso1 : 'Albator',perso2 : 'Ahmed Chawki ',perso3 : 'Amaterasu Okami ', desc : 'Ton personnage est-il un tueur ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+{text: { name: '420 personnage(s)',perso1 : 'Abigail Marston ',perso2 : 'Andrew Wiggins (personnage livre)',perso3 : 'Ahmed mansour ', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-green',collapsed : true, children : [
+{text: { name: '384 personnage(s)',perso1 : 'Albator',perso2 : 'Amadou Toumani Touré ',perso3 : 'Ahmed Chawki ', desc : 'Ton personnage est-il un tueur ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
- {text: { name: '342 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'Abel ',perso3 : 'Ahmed mansour ', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '341 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'Abel ',perso3 : 'Ahmed mansour ', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
 }, 
- {text: { name: '409 personnage(s)',perso1 : 'Abel ',perso2 : 'Ahmed mansour ',perso3 : 'Aissa Mandi	 ', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-red',collapsed : true, children : [
+ {text: { name: '408 personnage(s)',perso1 : 'Abel ',perso2 : 'Ahmed mansour ',perso3 : 'Aissa Mandi	 ', desc : 'Ton personnage a-t-il un prénom hors du commun ?'},HTMLclass :'light-red',collapsed : true, children : [
 {text: { name: '379 personnage(s)',perso1 : 'Abel ',perso2 : 'Ahmed Chawki ',perso3 : 'Ahmed mansour ', desc : 'Ton personnage fait-il des combats ?'},HTMLclass :'light-green',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
 }, 
- {text: { name: '333 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Ahmed Chawki ', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
+ {text: { name: '331 personnage(s)',perso1 : ' Samir Loussif ',perso2 : 'A rien lol',perso3 : 'Ahmed Chawki ', desc : 'Ton personnage est-il méchant ? '},HTMLclass :'light-red',collapsed : true, children : [{text : {name : 'Fin'}, collapsed: true}]
  } 
 ]
  } 
