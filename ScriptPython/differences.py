6fc8a0e7c5c88c7cfecd6db427ac06e21c29c733
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2020-05-20 14:32:59 +0200

    Modif Compare

--- ScriptPython/differences.py
+++ ScriptPython/differences.py
@@ -79,9 +79,9 @@ def differences(L1,L2):
     data=data.T 
     data["dif"]=dif
     for i in range(data.shape[0]) :
-        if(data.iloc[i,0]*data.iloc[i,1] < 0):
+        if(data.iloc[i,0]*data.iloc[i,1] < 0 and abs(data.iloc[i,0])>0.5 and abs(data.iloc[i,1])>0.5):
             data.iloc[i,2] += 10
-        elif (abs(data.iloc[i,0]) < 0.10 ) :
+        elif (abs(data.iloc[i,0]) < 0.20 and abs(data.iloc[i,1]) > 0.5) :
             data.iloc[i,2] += 5
     res = data.reindex(data["dif"].abs().sort_values(ascending = False).index)
     res= res.reset_index()
